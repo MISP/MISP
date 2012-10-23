cb677105b17e81a67432b495009f426fcb5d8ffe
noud <noud4@home.nl>
2012-10-23 15:00:48 +0200

    blackhole
    add component security to GroupsController.

--- app/Controller/GroupsController.php
+++ app/Controller/GroupsController.php
@@ -14,6 +14,7 @@ class GroupsController extends AppController {
                 'Actions' => array('actionPath' => 'controllers/Groups')
             )
         ),
+        'Security',
         'Session'
     );
 
