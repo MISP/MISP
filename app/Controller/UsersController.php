1c3ca8909b9e3a7b764c790a92d2b9d4ac720bbe
noud <noud4@home.nl>
2012-10-31 10:00:01 +0100

    Users
    invited by filled.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -161,6 +161,9 @@ class UsersController extends AppController {
 	public function admin_add() {
 		if ($this->request->is('post')) {
 			$this->User->create();
+			// set invited by
+			debug($this->request->data['User']);
+			$this->request->data['User']['invited_by'] = $this->Auth->user('id');
 			if ($this->User->save($this->request->data)) {
 				$this->Session->setFlash(__('The user has been saved'));
 				$this->redirect(array('action' => 'index'));
