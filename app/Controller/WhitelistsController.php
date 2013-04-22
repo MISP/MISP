31aefac5219f33448731de5dace83eb93e77d783
Christophe Vandeplas <christophe@vandeplas.com>
2013-04-22 16:45:32 +0200

    fix sanitization in Whitelist

--- app/Controller/WhitelistsController.php
+++ app/Controller/WhitelistsController.php
@@ -92,6 +92,6 @@ class WhitelistsController extends AppController {
  */
 	public function index() {
 		$this->recursive = 0;
-		$this->set('list', Sanitize::clean($this->paginate()));
+		$this->set('list', $this->paginate());
 	}
 }
