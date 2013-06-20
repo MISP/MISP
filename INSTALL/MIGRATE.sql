60f9621cd9f40fb55f801fa48d6cd81f373f3a6d
iglocska <andras.iklody@gmail.com>
2013-06-20 17:16:28 +0200

    Update to the migration

--- INSTALL/MIGRATE.sql
+++ INSTALL/MIGRATE.sql
@@ -71,3 +71,24 @@ CREATE TABLE IF NOT EXISTS `shadow_attributes` (
   KEY `old_id` (`old_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=11 ;
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `correlations`
+--
+
+DROP TABLE `correlations`;
+
+CREATE TABLE `correlations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` text COLLATE utf8_bin NOT NULL,
+  `1_event_id` int(11) NOT NULL,
+  `1_attribute_id` int(11) NOT NULL,
+  `1_private` tinyint(1) NOT NULL DEFAULT '0',
+  `event_id` int(11) NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `org` varchar(255) COLLATE utf8_bin NOT NULL,
+  `private` tinyint(1) NOT NULL,
+  `date` date NOT NULL,
+  `info` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
