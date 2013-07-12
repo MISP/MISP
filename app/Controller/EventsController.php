MISP/MISP

commit 2334599f3d460c4371597dc336749bebded459de

redirects to filtered events page upon delete

app/Controller/EventsController.php
@@ -746,7 +746,7 @@ class EventsController extends AppController {
 			//}
 
 			$this->Session->setFlash(__('Event deleted'));
-			$this->redirect(array('action' => 'index'));
+			$this->redirect($this->referer(array('action' => 'index')));
 		}
 		$this->Session->setFlash(__('Event was not deleted'));
 		$this->redirect(array('action' => 'index'));

commit b314a04e5816786ef11f3649f711cbf1526863a1

fixes bug introduced in commit 2334599f3d460c4371597dc336749bebded459de

app/Controller/EventsController.php
@@ -746,7 +746,13 @@ class EventsController extends AppController {
 			//}
 
 			$this->Session->setFlash(__('Event deleted'));
-			$this->redirect($this->referer(array('action' => 'index')));
+
+			// if coming from index, redirect to referer (to have the filter working)
+			// else redirect to index
+			if (strpos($this->referer(), '/view') !== FALSE)
+				$this->redirect(array('action' => 'index'));
+			else
+				$this->redirect($this->referer(array('action' => 'index')));
 		}
 		$this->Session->setFlash(__('Event was not deleted'));
 		$this->redirect(array('action' => 'index'));