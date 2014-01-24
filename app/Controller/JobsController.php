feff3551449c45beb6d6338d6de984aa8fee14c0
iglocska <andras.iklody@gmail.com>
2014-01-24 13:20:00 +0100

    incorrect argument passed to cache generation

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -91,7 +91,7 @@ class JobsController extends AppController {
 			$target = 'Events visible to: '.$this->Auth->user('org');
 			$jobOrg = $this->Auth->user('org');
 		}
-		$id = $this->Job->cache($type, $this->_isSiteAdmin(), $this->Auth->user('org'), $target, $jobOrg, $this->Auth->user('sid'));
+		$id = $this->Job->cache($type, $this->_isSiteAdmin(), $this->Auth->user('org'), $target, $jobOrg, $this->Auth->user('nids_sid'));
 		return new CakeResponse(array('body' => json_encode($id)));
 	}
 }
