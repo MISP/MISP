0330379eb5dd91ef10a05cd6366b49c6c01cf92b
Christophe Vandeplas <christophe@vandeplas.com>
2014-02-18 08:00:38 +0100

    fixing newlines in script

--- app/Console/worker/start.sh
+++ app/Console/worker/start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ../cake CakeResque.CakeResque stop --all
 ../cake CakeResque.CakeResque start --interval 5 --queue default
 ../cake CakeResque.CakeResque start --interval 5 --queue cache
