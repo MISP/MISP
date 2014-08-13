bde4ad074d3b9fbb2ae9b26c95000de8db549b5d
iglocska <andras.iklody@gmail.com>
2014-08-13 10:19:00 +0200

    Permission fix to the event filters
    Users could only choose their own organisation in the org filter due to an overly restrictive filtering of the available options. Relaxed to all organisations that have an event that is visible to the user.

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -271,7 +271,7 @@ class EventsController extends AppController {
 		}
 		$conditions = array();
 		if (!$this->_isSiteAdmin()) {
-			$conditions = array('OR' => array('orgc' => $this->Auth->User('org'), 'distribution' > 0));
+			$conditions = array('OR' => array(array('orgc' => $this->Auth->User('org')), array('distribution' > 0)));
 		}
 		$events = $this->Event->find('all', array(
 			'recursive' => -1,
