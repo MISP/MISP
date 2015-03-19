05bc4c4389326eff944db3b74f0d5b69599d04be
Richard van den Berg <richard.vandenberg@ncsc.nl>
2015-03-19 12:57:46 +0100

    Allow SyncTool to be used for generic HTTP(S) connections

--- app/Lib/Tools/SyncTool.php
+++ app/Lib/Tools/SyncTool.php
@@ -5,8 +5,10 @@ class SyncTool {
 	public function setupHttpSocket($server) {
 		$params = array();
 		App::uses('HttpSocket', 'Network/Http');
-		if ($server['Server']['cert_file'])	$params['ssl_cafile'] = APP . "files" . DS . "certs" . DS . $server['Server']['id'] . '.pem';
-		if ($server['Server']['self_signed']) $params['ssl_allow_self_signed'] = $server['Server']['self_signed'];
+		if(!empty($server['Server'])) {
+			if ($server['Server']['cert_file']) $params['ssl_cafile'] = APP . "files" . DS . "certs" . DS . $server['Server']['id'] . '.pem';
+			if ($server['Server']['self_signed']) $params['ssl_allow_self_signed'] = $server['Server']['self_signed'];
+		}
 		$HttpSocket = new HttpSocket($params);
 
 		$proxy = Configure::read('Proxy');
