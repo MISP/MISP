783170e3725921bf1429703ce81ec99ebd48964d
Guilherme Capilé <capile@tecnodz.com>
2015-04-12 16:32:57 -0300

    Added client SSL certificate authentication as a CakePHP plugin

--- app/Config/config.default.php
+++ app/Config/config.default.php
@@ -6,6 +6,7 @@ $config = array (
     'level' => 'medium',
     'salt' => 'Rooraenietu8Eeyo<Qu2eeNfterd-dd+',
     'cipherSeed' => '',
+    //'auth'=>array('CertAuth.Certificate'), // additional authentication methods
   ),
   'MISP' => 
   array (
@@ -39,4 +40,36 @@ $config = array (
     'amount' => 5,
     'expire' => 300,
   ),
+  // Uncomment the following to enable client SSL certificate authentication
+  /*
+  'CertAuth' => 
+  array(
+    'register'=>true,
+    'ca'=>'FIRST.Org',
+    'caId'=>'org',
+    'mapCa'=>array(
+      'O'=>'org',
+      'emailAddress'=>'email',
+    ),
+    'map'=>array(
+      'O'=>'org',
+      'emailAddress'=>'email',
+    ),
+    'userModel'=>'User',
+    'userModelKey'=>'nids_sid',
+    'enableSession' => true,
+    'syncUser'=>true,
+    'restApi'=>array(
+      'url'=>'https://www.first.org/data/members?scope=full&limit=1',
+      'headers'=>array(),
+      'param'=>array('email'=>'email'),
+      'map'=>array(
+        'uid'=>'nids_sid',
+        'team'=>'org',
+        'email'=>'email',
+        'pgp_public'=>'gpgkey',
+      ),
+    ),
+  ),
+  */
 );
