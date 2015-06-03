b200cbb83ac0fe1b3969b8391c6de97abde12f97
Iglocska <andras.iklody@gmail.com>
2015-06-03 11:38:19 +0200

    AJAX attribute creation would block a follow-up publish request, fixes #514
    - Popover_form purged after the form has been submitted
    - a duplicate hidden div was created for confirmation popups within the attribute creation popup and clicking publish populated the wrong div

--- app/webroot/js/ajaxification.js
+++ app/webroot/js/ajaxification.js
@@ -14,9 +14,9 @@ function publishPopup(id, type) {
 	if (type == "publish") action = "publish";
 	var destination = 'attributes';
 	$.get( "/events/" + action + "/" + id, function(data) {
+		$("#confirmation_box").html(data);
 		$("#confirmation_box").fadeIn();
 		$("#gray_out").fadeIn();
-		$("#confirmation_box").html(data);
 	});
 }
 
@@ -505,6 +505,7 @@ function submitPopoverForm(context_id, referer, update_context_id) {
 			type:"post", 
 			url:url
 		});
+		$("#popover_form").empty();
 	}
 };
 
