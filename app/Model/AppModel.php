c104a1763b68fd4ccd92a26202ba260e0c6a56fa
Iglocska <andras.iklody@gmail.com>
2015-11-26 04:38:57 +0100

    Temporarily re-added org field for jobs

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -112,6 +112,8 @@ class AppModel extends Model {
 				KEY `taxonomy_id` (`taxonomy_id`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
 				
+				$sqlArray[] = 'ALTER TABLE `jobs` ADD  `org` text COLLATE utf8_bin NOT NULL';
+				
 				break;
 			default:
 				return false;
