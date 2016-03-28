eed7bb17b24d4fc3ecb3e3c5248bf09519223a39
Iglocska <andras.iklody@gmail.com>
2016-03-28 09:01:30 +0200

    Added logging to the blacklist models

--- app/Model/EventBlacklist.php
+++ app/Model/EventBlacklist.php
@@ -4,6 +4,10 @@ class EventBlacklist extends AppModel{
 	public $useTable = 'event_blacklists';
 	public $recursive = -1;
 	public $actsAs = array(
+			'SysLogLogable.SysLogLogable' => array(	// TODO Audit, logable
+					'userModel' => 'User',
+					'userKey' => 'user_id',
+					'change' => 'full'),
 			'Containable',
 	);
 	public $blacklistFields = array('event_uuid', 'comment', 'event_info', 'event_orgc');
