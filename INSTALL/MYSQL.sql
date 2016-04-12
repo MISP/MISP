7801a8ea6db257fbf622c47b8f1ab7734b312d19
Steve Fossen <sfossen@gmail.com>
2016-04-12 13:31:58 -0700

    defaults for logs table.

--- INSTALL/MYSQL.sql
+++ INSTALL/MYSQL.sql
@@ -209,16 +209,16 @@ CREATE TABLE IF NOT EXISTS `jobs` (
 
 CREATE TABLE IF NOT EXISTS `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `created` datetime NOT NULL,
   `model` varchar(20) COLLATE utf8_bin NOT NULL,
   `model_id` int(11) NOT NULL,
   `action` varchar(20) COLLATE utf8_bin NOT NULL,
   `user_id` int(11) NOT NULL,
-  `change` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `change` text CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `org` varchar(255) COLLATE utf8_bin NOT NULL,
-  `description` varchar(255) COLLATE utf8_bin NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
