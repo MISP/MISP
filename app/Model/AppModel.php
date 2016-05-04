53d1f05362978698f869b9cb7502789c6953d6df
Iglocska <andras.iklody@gmail.com>
2016-05-04 09:10:42 +0200

    chg: DB changes for the attribute deletion

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -49,7 +49,7 @@ class AppModel extends Model {
 	// major -> minor -> hotfix -> requires_logout
 	public $db_changes = array(
 		2 => array(
-			4 => array(18 => false, 19 => false, 20 => false, 25 => false, 27 => false, 32 => false, 33 => true, 38 => true, 39 => true, 40 => false)
+			4 => array(18 => false, 19 => false, 20 => false, 25 => false, 27 => false, 32 => false, 33 => true, 38 => true, 39 => true, 40 => false, 42 => false)
 		)
 	);
 	
@@ -362,6 +362,9 @@ class AppModel extends Model {
 					INDEX `user_id` (`user_id`),
 					INDEX `tag_id` (`tag_id`)
 					) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+				break;
+			case '2.4.42':
+				$sqlArray[] = "ALTER TABLE `attributes` ADD `deleted` tinyint(1) NOT NULL DEFAULT '0';";
 				break;
 			case 'fixNonEmptySharingGroupID':
 				$sqlArray[] = 'UPDATE `events` SET `sharing_group_id` = 0 WHERE `distribution` != 4';
