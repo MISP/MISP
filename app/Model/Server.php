12ba687fddb48cb5f9247a868e8a504a2c398a1c
Steve Fossen <sfossen@gmail.com>
2016-05-26 15:24:49 -0700

    typo

--- app/Model/Server.php
+++ app/Model/Server.php
@@ -1177,7 +1177,7 @@ class Server extends AppModel {
 							$r = $this->EventBlacklist->find('first', array('conditions' => array('event_uuid' => $event['Event']['uuid'])));
 							if (!empty($r))	{
 								$blocked = true;
-								$fails[$eventId] = 'Event blocked by local blocklist.';
+								$fails[$eventId] = 'Event blocked by local blacklist.';
 							}
 						}
 						if (!$blocked) {
