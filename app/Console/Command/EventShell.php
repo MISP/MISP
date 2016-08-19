024be2d08f0911570b99cbe5fed31eba406e447e
ppanero <ppanero@visitor-14913781.dyndns.cern.ch>
2016-08-19 15:14:39 +0200

    EventShell controller for bro export, cachebro function implemented

--- app/Console/Command/EventShell.php
+++ app/Console/Command/EventShell.php
@@ -263,6 +263,50 @@ class EventShell extends AppShell
 		$this->Job->saveField('date_modified', date("y-m-d H:i:s"));
 	}
 
+	public function cachebro()
+	{
+		$broHeader = "#fields indicator\tindicator_type\tmeta.source\tmeta.url\tmeta.do_notice\tmeta.if_in\n";
+		$userId = $this->args[0];
+		$user = $this->User->getAuthUser($userId);
+		$id = $this->args[1];
+		$this->Job->id = $id;
+		$format = $this->args[2];
+		$this->Job->saveField('progress', 1);
+		//$types = array_keys($this->Attribute->typeDefinitions);
+		$types = array('ip', 'email', 'domain', 'filename', 'filehash', 'certhash', 'software', 'url'); //Bro types
+		$typeCount = count($types);
+		$dir = new Folder(APP . DS . '/tmp/cached_exports/' . $format, true, 0750);
+
+		if ($user['Role']['perm_site_admin']) {
+			$zipname = DS . 'misp.bro.ADMIN.intel.zip';
+		} else {
+			$zipname = DS . 'misp.bro.' . $user['Organisation']['name'] . '.intel.zip';
+		}
+		$zip = new ZipArchive();
+		if ($zip->open($dir->pwd() . $zipname, ZIPARCHIVE::CREATE) === TRUE) {
+			foreach ($types as $k => $type) {
+
+				$final = $this->Attribute->bro($user, $type);
+				if ($user['Role']['perm_site_admin']) {
+					$filename = DS . 'misp.' . $type . '.ADMIN.intel';
+				} else {
+					$filename = DS . 'misp.' . $type . '.' . $user['Organisation']['name'] . '.intel';
+				}
+				$file = new File($dir->pwd() . $filename);
+				$file->write($broHeader);
+				foreach ($final as $attribute) {
+					$file->append($attribute . "\n");
+				}
+				$file->close();
+				$zip->addFile($dir->pwd() . $filename, $type . '.intel');
+				//unlink($dir->pwd() . $filename);
+				$this->Job->saveField('progress', $k / $typeCount * 100);
+			}
+		}
+		$this->Job->saveField('progress', 100);
+		$this->Job->saveField('message', 'Job done.');
+	}
+	
 	public function alertemail() {
 		$userId = $this->args[0];
 		$processId = $this->args[1];
