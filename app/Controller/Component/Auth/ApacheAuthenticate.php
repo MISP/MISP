e6585ed46e069ef2196f5c60a73a95a8810dbc41
Andreas Ziegler <github@andreas-ziegler.de>
2016-08-23 04:27:28 +0200

    chg: fix indention of 4 files

--- app/Controller/Component/Auth/ApacheAuthenticate.php
+++ app/Controller/Component/Auth/ApacheAuthenticate.php
@@ -16,99 +16,99 @@ App::uses('BaseAuthenticate', 'Controller/Component/Auth');
 
 class ApacheAuthenticate extends BaseAuthenticate {
 
-    /**
-     * Authentication class
-     *
-     * @param CakeRequest $request The request that contains login information.
-     * @param CakeResponse $response Unused response object.
-     * @return mixed False on login failure. An array of User data on success.
-     */
-    public function authenticate(CakeRequest $request, CakeResponse $response) {
+	/**
+	 * Authentication class
+	 *
+	 * @param CakeRequest $request The request that contains login information.
+	 * @param CakeResponse $response Unused response object.
+	 * @return mixed False on login failure. An array of User data on success.
+	 */
+	public function authenticate(CakeRequest $request, CakeResponse $response) {
 
-        // Get information user for MISP auth
-        $envvar = $this->settings['fields']['envvar'];
-        $mispUsername = $_SERVER[$envvar];
+		// Get information user for MISP auth
+		$envvar = $this->settings['fields']['envvar'];
+		$mispUsername = $_SERVER[$envvar];
 
-        // make LDAP request to get user email required for MISP auth
-        $ldapdn = Configure::read('ApacheSecureAuth.ldapDN');
-        $ldaprdn = Configure::read('ApacheSecureAuth.ldapReaderUser');     // DN ou RDN LDAP
-        $ldappass = Configure::read('ApacheSecureAuth.ldapReaderPassword');
+		// make LDAP request to get user email required for MISP auth
+		$ldapdn = Configure::read('ApacheSecureAuth.ldapDN');
+		$ldaprdn = Configure::read('ApacheSecureAuth.ldapReaderUser');     // DN ou RDN LDAP
+		$ldappass = Configure::read('ApacheSecureAuth.ldapReaderPassword');
 
-        // LDAP connection
-        $ldapconn = ldap_connect(Configure::read('ApacheSecureAuth.ldapServer'))
-                or die('LDAP server connection failed');
+		// LDAP connection
+		$ldapconn = ldap_connect(Configure::read('ApacheSecureAuth.ldapServer'))
+				or die('LDAP server connection failed');
 
-        // LDAP protocol configuration
-        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, Configure::read('ApacheSecureAuth.ldapProtocol'));
+		// LDAP protocol configuration
+		ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, Configure::read('ApacheSecureAuth.ldapProtocol'));
 
-        if ($ldapconn) {
-            // LDAP bind
-            $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
-            // authentication verification
-            if (!$ldapbind) {
-                die("LDAP bind failed");
-            }
-            // example: '(uuid=ApacheUser)'
-            $filter = '('.Configure::read('ApacheSecureAuth.ldapSearchAttribut').'=' . $_SERVER[$envvar] . ')';
-            // example: mail
-            $getLdapUserInfo = Configure::read('ApacheSecureAuth.ldapFilter');
+		if ($ldapconn) {
+			// LDAP bind
+			$ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+			// authentication verification
+			if (!$ldapbind) {
+				die("LDAP bind failed");
+			}
+			// example: '(uuid=ApacheUser)'
+			$filter = '('.Configure::read('ApacheSecureAuth.ldapSearchAttribut').'=' . $_SERVER[$envvar] . ')';
+			// example: mail
+			$getLdapUserInfo = Configure::read('ApacheSecureAuth.ldapFilter');
 
-            $result = ldap_search($ldapconn, $ldapdn, $filter, $getLdapUserInfo)
-                    or die("Error in LDAP search query: " . ldap_error($ldapconn));
+			$result = ldap_search($ldapconn, $ldapdn, $filter, $getLdapUserInfo)
+					or die("Error in LDAP search query: " . ldap_error($ldapconn));
 
-            $ldapUserData = ldap_get_entries($ldapconn, $result);
+			$ldapUserData = ldap_get_entries($ldapconn, $result);
 
-            // the request returns only 1 field
-            if (isset($ldapUserData[0]['mail'][0])) {
-                // assign the real user for MISP
-                $mispUsername = $ldapUserData[0]['mail'][0];
-            } else {
-                die("User not found in LDAP");
-            }
-            // close LDAP connection
-            ldap_close($ldapconn);
-        }
+			// the request returns only 1 field
+			if (isset($ldapUserData[0]['mail'][0])) {
+				// assign the real user for MISP
+				$mispUsername = $ldapUserData[0]['mail'][0];
+			} else {
+				die("User not found in LDAP");
+			}
+			// close LDAP connection
+			ldap_close($ldapconn);
+		}
 
-        // Find user with real username (mail)
-        $user = $this->_findUser($mispUsername);
+		// Find user with real username (mail)
+		$user = $this->_findUser($mispUsername);
 
-        if ($user) {
-            return $user;
-        }
+		if ($user) {
+			return $user;
+		}
 
-        // insert user in database if not existent
-        $userModel = ClassRegistry::init($this->settings['userModel']);
-        $org_id = Configure::read('ApacheSecureAuth.ldapDefaultOrg');
-        // If not in config, take default org
-        if (!isset($org_id)) {
-            $firstOrg = $userModel->Organisation->find(
-                    'first', array(
-                        'conditions' => array(
-                            'Organisation.local' => true),
-                        'order' => 'Organisation.id ASC'
-                    )
-            );
-            $org_id = $firstOrg['Organisation']['id'];
-        }
+		// insert user in database if not existent
+		$userModel = ClassRegistry::init($this->settings['userModel']);
+		$org_id = Configure::read('ApacheSecureAuth.ldapDefaultOrg');
+		// If not in config, take default org
+		if (!isset($org_id)) {
+			$firstOrg = $userModel->Organisation->find(
+				'first', array(
+					'conditions' => array(
+						'Organisation.local' => true),
+					'order' => 'Organisation.id ASC'
+				)
+			);
+			$org_id = $firstOrg['Organisation']['id'];
+		}
 
-        // create user
-        $userData = array('User' => array(
-                'email' => $mispUsername,
-                'org_id' => $org_id,
-                'password' => '',
-                'confirm_password' => '',
-                'authkey' => $userModel->generateAuthKey(),
-                'nids_sid' => 4000000,
-                'newsread' => date('Y-m-d'),
-                'role_id' => Configure::read('ApacheSecureAuth.ldapDefaultRoleId'),
-                'change_pw' => 0
-        ));
-        // save user
-        $userModel->save($userData, false);
+		// create user
+		$userData = array('User' => array(
+			'email' => $mispUsername,
+			'org_id' => $org_id,
+			'password' => '',
+			'confirm_password' => '',
+			'authkey' => $userModel->generateAuthKey(),
+			'nids_sid' => 4000000,
+			'newsread' => date('Y-m-d'),
+			'role_id' => Configure::read('ApacheSecureAuth.ldapDefaultRoleId'),
+			'change_pw' => 0
+		));
+		// save user
+		$userModel->save($userData, false);
 
-        return $this->_findUser(
-                        $mispUsername
-        );
-    }
+		return $this->_findUser(
+			$mispUsername
+		);
+	}
 
 }
