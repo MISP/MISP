09b05aaaaade43225cfa2e39b6e899d5e92c3997
Iglocska <andras.iklody@gmail.com>
2016-09-22 11:45:00 +0200

    fix: don't load relations when running the password shell

--- app/Console/Command/PasswordShell.php
+++ app/Console/Command/PasswordShell.php
@@ -13,11 +13,10 @@ class PasswordShell extends AppShell {
 		if (!isset($this->args[0]) || empty($this->args[0]) || !isset($this->args[1]) || empty($this->args[1])) echo 'MISP password reset command line tool.' . PHP_EOL . 'To assign a new password for a user:' . PHP_EOL . APP . 'Console/cake Password [email] [password]' . PHP_EOL;
 		else {
 			// get the users that need their password hashed
-			$results = $this->User->find('first', array('conditions' => array('email' => $this->args[0])));
+			$results = $this->User->find('first', array('conditions' => array('email' => $this->args[0]), 'recursive' => -1));
 			$results['User']['password'] = $this->args[1];
 			$results['User']['confirm_password'] = $this->args[1];
 			$results['User']['change_pw'] = 1;
-
 			if (!$this->User->save($results)) {
 				echo 'Could not update account for User.id = ', $results['User']['id'], PHP_EOL;
 				debug($this->User->validationErrors);
