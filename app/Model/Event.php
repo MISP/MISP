9215926d37fac157f3cf994afb7d59436bc91e28
Iglocska <andras.iklody@gmail.com>
2016-12-02 15:47:02 +0100

    fix: Do not allow empty values to be returned by the enrichment queries

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -2864,7 +2864,11 @@ class Event extends AppModel {
 						$r['values'] = array($r['values']);
 					}
 				}
-				foreach ($r['values'] as &$value) {
+				foreach ($r['values'] as $valueKey => &$value) {
+					if (empty($value)) {
+						unset($r['values'][$valueKey]);
+						continue;
+					}
 					if (in_array('freetext', $r['types'])) {
 						if (is_array($value)) $value = json_encode($value);
 						$this->Warninglist = ClassRegistry::init('Warninglist');
