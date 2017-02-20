a59aab9b236403d3e931b102b17c089ee705e7a0
iglocska <andras.iklody@gmail.com>
2017-02-20 18:43:36 +0100

    fix: Re-added the accidentally removed code in a merge, fixes #1965
    - affects f0e1a27b7dca2e6d36f904ef52d4976649ccefa3

--- app/Model/Feed.php
+++ app/Model/Feed.php
@@ -655,12 +655,20 @@ class Feed extends AppModel {
 		if (empty($data)) {
 			return true;
 		}
+		$prunedCopy = array();
 		foreach ($data as $key => $value) {
+			foreach ($prunedCopy as $copy) {
+ 				if ($copy['type'] == $value['type'] && $copy['category'] == $value['category'] && $copy['value'] == $value['value']) {
+					continue 2;
+				}
+			}
 			$data[$key]['event_id'] = $event['Event']['id'];
 			$data[$key]['distribution'] = $feed['Feed']['distribution'];
 			$data[$key]['sharing_group_id'] = $feed['Feed']['sharing_group_id'];
 			$data[$key]['to_ids'] = $feed['Feed']['override_ids'] ? 0 : $data[$key]['to_ids'];
+			$prunedCopy[] = $data[$key];
 		}
+		$data = $prunedCopy;
 		if ($jobId) {
 			$job = ClassRegistry::init('Job');
 			$job->id = $jobId;
--- app/View/Whitelists/admin_index.ctp
+++ app/View/Whitelists/admin_index.ctp
@@ -1,5 +1,6 @@
 <div class="whitelist index">
-	<h2>Import Whitelist</h2>
+	<h2>Signature Whitelist</h2>
+	<p>Regex entries (in the standard php regex /{regex}/{modifier} format) entered below will restrict matching attributes from being included in the IDS flag sensitive exports (such as NIDS exports).</p>
 	<div class="pagination">
         <ul>
         <?php
