48561722bd5249fdaf6bbd41308a22fac555a484
Pablo Panero <ppanero27@gmail.com>
2017-02-23 12:34:05 +0100

    Update Server.php
    Duplicate entry of property

--- app/Model/Server.php
+++ app/Model/Server.php
@@ -735,15 +735,6 @@ class Server extends AppModel {
 							'type' => 'boolean',
 							'null' => true
 					),
-					'showCorrelationsOnIndex' => array(
-							'level' => 1,
-							'description' => 'When enabled, the number of correlations visible to the currently logged in user will be visible on the event index UI. This comes at a performance cost but can be very useful to see correlating events at a glance.',
-							'value' => false,
-							'errorMessage' => '',
-							'test' => 'testBool',
-							'type' => 'boolean',
-							'null' => true
-					),
 			),
 			'GnuPG' => array(
 					'branch' => 1,
