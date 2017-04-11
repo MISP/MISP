10de1891d2f681c38749e387bad854405d71f647
iglocska <andras.iklody@gmail.com>
2017-04-11 11:28:25 +0200

    fix: Cleanup of the role add/edit checkboxes

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility');
 
-	private $__queryVersion = '4';
+	private $__queryVersion = '5';
 	public $pyMispVersion = '2.4.68';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.0';
