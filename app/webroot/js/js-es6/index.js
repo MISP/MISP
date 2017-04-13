0235904030393c897edefeedd094f0ad48f73eb8
iglocska <andras.iklody@gmail.com>
2017-04-13 16:31:48 +0200

    fix: reverted JS changes for now

--- app/webroot/js/js-es6/index.js
+++
@@ -1,7 +0,0 @@
-window.MISPUI = require("./misp-ui");
-
-var functions = Object.keys(MISPUI);
-for (var func in functions) {
-    var funcName = functions[func];
-    window[funcName] = MISPUI[funcName];
-}
