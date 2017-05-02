9c349caa443631fdfe92cb949c6d85ae3e08778e
Tristan METAYER <tristan.metayer@ssi.gouv.fr>
2017-05-02 10:56:21 +0200

    Add filename key for import modules

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -3980,6 +3980,7 @@ class EventsController extends AppController {
 				}
 				if (!$fail) {
 					$modulePayload['data'] = base64_encode($modulePayload['data']);
+					$modulePayload['filename'] = $filename;
 					$result = $this->Module->queryModuleServer('/query', json_encode($modulePayload, true), false, $moduleFamily = 'Import');
 					if (!$result) throw new Exception('Import service not reachable.');
 					if (isset($result['error'])) $this->Session->setFlash($result['error']);
