165266b072688576c56569e51228617749b4d4a1
iglocska <andras.iklody@gmail.com>
2017-05-28 00:47:38 +1000

    fix: Some cleanup in the mispzmq script

--- app/files/scripts/mispzmq/mispzmq.py
+++ app/files/scripts/mispzmq/mispzmq.py
@@ -68,17 +68,14 @@ def main(args):
         time.sleep(1)
         command = r.lpop(namespace + ":command")
         if command is not None:
-            print(command)
             handleCommand(command)
         topics = ["misp_json", "misp_json_attribute", "misp_json_sighting"]
-        test = 0
         for topic in topics:
             data = r.lpop(namespace + ":data:" + topic)
+            print(data)
             if data is None:
                 continue
-            bla = '1'
             pubMessage(topic, data)
-            test = test+1
 
 if __name__ == "__main__":
     main(sys.argv)
