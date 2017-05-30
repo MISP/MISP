1efe4a89cebfbf410a17df307cc30d6bd93ccf37
iglocska <andras.iklody@gmail.com>
2017-05-30 18:10:24 +0200

    chg: Querystring version bump

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility');
 
-	private $__queryVersion = '10';
+	private $__queryVersion = '11';
 	public $pyMispVersion = '2.4.71';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
