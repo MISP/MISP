3e3507a5beccf4d35b5d7949ae9367a7458241f6
iglocska <andras.iklody@gmail.com>
2017-06-15 10:00:35 +0200

    fix: Don't return the mixbox version if no mixbox is installed

--- app/files/scripts/stixtest.py
+++ app/files/scripts/stixtest.py
@@ -3,11 +3,12 @@ try:
     import stix
     import cybox
 except ImportError:
-    print json.dumps({'success' : 0})
+
+    print(json.dumps({'success' : 0}))
     sys.exit(1)
 try:
     import mixbox
-    print json.dumps({'success' : 1, 'stix' : stix.__version__, 'cybox' : cybox.__version__, 'mixbox' : mixbox.__version__})
+    print(json.dumps({'success' : 1, 'stix' : stix.__version__, 'cybox' : cybox.__version__, 'mixbox' : mixbox.__version__}))
 except ImportError:
-    print json.dumps({'success' : 1, 'stix' : stix.__version__, 'cybox' : cybox.__version__, 'mixbox' : 0})
+    print(json.dumps({'success' : 1, 'stix' : stix.__version__, 'cybox' : cybox.__version__, 'mixbox' : 0}))
 sys.exit(1)
