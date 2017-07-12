f4041cd100b40be771bed3828d3f3f92cc594fd9
iglocska <andras.iklody@gmail.com>
2017-07-12 16:16:20 +0200

    fix: Updated pyMisp and querystring versions

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,8 +46,8 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility');
 
-	private $__queryVersion = '13';
-	public $pyMispVersion = '2.4.71';
+	private $__queryVersion = '14';
+	public $pyMispVersion = '2.4.77';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
 
