0097e040b182a961c8abf21960f8b46cd97e5cde
iglocska <andras.iklody@gmail.com>
2017-08-01 01:05:45 +0200

    fix: Previous commit was incorrect, empty filters contain null not false

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -2640,7 +2640,7 @@ class EventsController extends AppController {
 			if ($publish_timestamp) $conditions = $this->Event->Attribute->setPublishTimestampConditions($publish_timestamp, $conditions);
 			if ($timestamp) $conditions = $this->Event->Attribute->setTimestampConditions($timestamp, $conditions);
 			if ($last) $conditions['AND'][] = array('Event.publish_timestamp >=' => $last);
-			if ($published !== false) $conditions['AND'][] = array('Event.published' => $published);
+			if ($published !== null) $conditions['AND'][] = array('Event.published' => $published);
 			$params = array(
 					'conditions' => $conditions,
 					'fields' => array('DISTINCT(Attribute.event_id)'),
