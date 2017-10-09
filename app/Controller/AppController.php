22804d2eacc58af34c2781e6f00743faf61a8d53
iglocska <andras.iklody@gmail.com>
2017-10-09 16:20:57 +0200

    fix: PyMISP and warninglists updated

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -47,7 +47,7 @@ class AppController extends Controller {
 	public $helpers = array('Utility');
 
 	private $__queryVersion = '22';
-	public $pyMispVersion = '2.4.80.1';
+	public $pyMispVersion = '2.4.81';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
 
