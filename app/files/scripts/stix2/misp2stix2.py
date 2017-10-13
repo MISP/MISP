3b3b1b42c5fcdfcbfa9d689068148054e7f4080b
chrisr3d <chris.studer.68@gmail.com>
2017-10-13 16:31:33 +0200

    Added malware-sample case
    Also fixed some 'pattern' fields in the dictionary

--- app/files/scripts/stix2/misp2stix2.py
+++ app/files/scripts/stix2/misp2stix2.py
@@ -27,19 +27,19 @@ non_indicator_attributes = ['text', 'comment', 'other', 'link', 'target-user', '
                             'vulnerability', 'attachment']
 
 mispTypesMapping = {'md5': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                            'pattern': 'file:hashes.\'{1}\' = \'{0}\''},
+                            'pattern': 'file:hashes.\'md5\' = \'{0}\''},
                     'sha1': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                             'pattern': 'file:hashes.\'{1}\' = \'{0}\''},
+                             'pattern': 'file:hashes.\'sha1\' = \'{0}\''},
                     'sha256': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{1}\' = \'{0}\''},
+                               'pattern': 'file:hashes.\'sha256\' = \'{0}\''},
                     'filename': {'observable': {'0': {'type': 'file', 'name': ''}},
                                  'pattern': 'file:name = \'{0}\''},
                     'filename|md5': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                     'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                     'pattern': 'file:name = \'{0}\' AND file:hashes.\'md5\' = \'{1}\''},
                     'filename|sha1': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                      'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                      'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha1\' = \'{1}\''},
                     'filename|sha256': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha256\' = \'{1}\''},
                     'ip-src': {'observable': {'0': {'type': '', 'value': ''}},
                                'pattern': '{0}:value = \'{1}\''},
                     'ip-dst': {'observable': {'0': {'type': '', 'value': ''}},
@@ -65,54 +65,56 @@ mispTypesMapping = {'md5': {'observable': {'0': {'type': 'file', 'hashes': ''}},
                                'pattern': 'windows-registry-key:key = \'{0}\''},
                     'regkey|value': {'observable': {'0': {'type': 'windows-registry-key', 'key': '', 'values': {'name': ''}}},
                                      'pattern': 'windows-registry-key:key = \'{0}\' AND windows-registry-key:values = \'{1}\''},
+                    'malware-sample': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
+                                       'pattern': 'file:name = \'{0}\' AND file:hashes.\'md5\' = \'{1}\''},
                     'mutex': {'observable': {'0': {'type': 'mutex', 'name': ''}},
                               'pattern': 'mutex:name = \'{0}\''},
                     'uri': {'observable': {'0': {'type': 'url', 'value': ''}},
                             'pattern': 'url:value = \'{0}\''},
                     'authentihash': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                                     'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                                     'pattern': 'file:hashes.\'authentihash\' = \'{0}\''},
                     'ssdeep': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                               'pattern': 'file:hashes.\'ssdeep\' = \'{0}\''},
                     'imphash': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                                'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                                'pattern': 'file:hashes.\'imphash\' = \'{0}\''},
                     'pehash': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                               'pattern': 'file:hashes.\'pehash\' = \'{0}\''},
                     'impfuzzy': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                                 'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                                 'pattern': 'file:hashes.\'impfuzzy\' = \'{0}\''},
                     'sha224': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                               'pattern': 'file:hashes.\'sha224\' = \'{0}\''},
                     'sha384': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                               'pattern': 'file:hashes.\'sha384\' = \'{0}\''},
                     'sha512': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                               'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                               'pattern': 'file:hashes.\'sha512\' = \'{0}\''},
                     'sha512/224': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                                   'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                                   'pattern': 'file:hashes.\'sha512/224\' = \'{0}\''},
                     'sha512/256': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                                   'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                                   'pattern': 'file:hashes.\'sha512/256\' = \'{0}\''},
                     'tlsh': {'observable': {'0': {'type': 'file', 'hashes': ''}},
-                             'pattern': 'file:hashes.\'{2}\' = \'{1}\''},
+                             'pattern': 'file:hashes.\'tlsh\' = \'{0}\''},
                     'filename|authentihash': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                              'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                              'pattern': 'file:name = \'{0}\' AND file:hashes.\'authentihash\' = \'{1}\''},
                     'filename|ssdeep': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'ssdeep\' = \'{1}\''},
                     'filename|imphash': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                         'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                         'pattern': 'file:name = \'{0}\' AND file:hashes.\'imphash\' = \'{1}\''},
                     'filename|impfuzzy': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                          'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                          'pattern': 'file:name = \'{0}\' AND file:hashes.\'impfuzzy\' = \'{1}\''},
                     'filename|pehash': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'pehash\' = \'{1}\''},
                     'filename|sha224': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha224\' = \'{1}\''},
                     'filename|sha384': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha384\' = \'{1}\''},
                     'filename|sha512': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                        'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha512\' = \'{1}\''},
                     'filename|sha512/224': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                            'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                            'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha512/224\' = \'{1}\''},
                     'filename|sha512/256': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                            'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                            'pattern': 'file:name = \'{0}\' AND file:hashes.\'sha512/256\' = \'{1}\''},
                     'filename|tlsh': {'observable': {'0': {'type': 'file', 'name': '', 'hashes': ''}},
-                                      'pattern': 'file:name = \'{0}\' AND file:hashes.\'{3}\' = \'{1}\''},
+                                      'pattern': 'file:name = \'{0}\' AND file:hashes.\'tlsh\' = \'{1}\''},
                     'x509-fingerprint-sha1': {'observable': {'0': {'type': 'x509-certificate', 'hashes': {'sha1': ''}}},
                                               'pattern': 'x509-certificate:hashes = \'{0}\''},
                     'port': {'observable': {'0': {'type': 'network-traffic', 'dst_port': ''}},
@@ -362,7 +364,11 @@ def defineObservableObject(attr_type, attr_val):
             object0['values']['name'] = attr_val2
         else:
             object0['name'] = attr_val1
-            object0['hashes'] = {attr_type2: attr_val2}
+            object0['hashes'] = {attr_type2: attr_val2}      
+    elif attr_type == 'malware-sample':
+        attr_val1, attr_val2 = attr_val.split('|')
+        object0['name'] = attr_val1
+        object0['hashes'] = {'md5': attr_val2}
     else:
         if 'x509' in attr_type:
             object0['hashes']['sha1'] = attr_val
@@ -385,13 +391,16 @@ def definePattern(attr_type, attr_val):
             addr_type = defineAddressType(attr_val2)
             pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val1, attr_val2, addr_type)
         else:
-            pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val1, attr_val2, attr_type1, attr_type2)
+            pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val1, attr_val2)
+    elif attr_type == 'malware-sample':
+        attr_val1, attr_val2 = attr_val.split('|')
+        pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val1, attr_val2)
     else:
         if 'ip-' in attr_type:
             addr_type = defineAddressType(attr_val)
             pattern = mispTypesMapping[attr_type]['pattern'].format(addr_type, attr_val)
         else:
-            pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val, attr_type)
+            pattern = mispTypesMapping[attr_type]['pattern'].format(attr_val)
     return pattern
 
 def defineAddressType(attr_val):
