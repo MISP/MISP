6cd0a29f02bdf7713f2153eb5cc24a0165b34c4b
iglocska <andras.iklody@gmail.com>
2017-10-27 16:38:39 +0200

    fix: Fixed the CSV field name for date

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -2302,6 +2302,9 @@ class EventsController extends AppController {
 		}
 		$headers = array_merge($requested_attributes, $requested_obj_attributes);
 		if ($includeContext) $headers = array_merge($headers, array_keys($this->Event->csv_event_context_fields_to_fetch));
+		foreach ($headers as $k => $v) {
+			if ($v == 'timestamp') $headers[$k] = 'date';
+		}
 		$headers = implode(',', $headers);
 		$final = array_merge(array($headers), $final);
 		$final = implode(PHP_EOL, $final);
