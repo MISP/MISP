f7418f2d6c9494b32ba3f6d7055afe37b7e85741
iglocska <andras.iklody@gmail.com>
2017-11-10 07:53:19 +0100

    chg: PyMISP version bump

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,8 +46,8 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility');
 
-	private $__queryVersion = '22';
-	public $pyMispVersion = '2.4.81';
+	private $__queryVersion = '23';
+	public $pyMispVersion = '2.4.82';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
 
