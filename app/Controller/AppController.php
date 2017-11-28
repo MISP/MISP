2910e16c3b5430fbd62b7308ce2654d7f95da6bc
iglocska <andras.iklody@gmail.com>
2017-11-28 11:45:16 +0100

    fix: Fixed an issue where proposal quick edits didn't work for normal users, fixes #2685

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility');
 
-	private $__queryVersion = '24';
+	private $__queryVersion = '25';
 	public $pyMispVersion = '2.4.82';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
