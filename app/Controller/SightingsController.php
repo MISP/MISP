63ee8eaa8198d0ac4712a7d6cd63810304e550cd
iglocska <andras.iklody@gmail.com>
2018-01-18 07:49:08 +0100

    fix: Add flatten to advanced sightings add within objects
    - without the flattening the advanced sighting add functionality couldn't be loaded

--- app/Controller/SightingsController.php
+++ app/Controller/SightingsController.php
@@ -78,7 +78,7 @@ class SightingsController extends AppController {
 			} else {
 				$this->layout = false;
 				$this->loadModel('Attribute');
-				$attributes = $this->Attribute->fetchAttributes($this->Auth->user(), array('conditions' => array('Attribute.id' => $id)));
+				$attributes = $this->Attribute->fetchAttributes($this->Auth->user(), array('conditions' => array('Attribute.id' => $id), 'flatten' => 1));
 				if (empty($attributes)) {
 					throw new MethodNotAllowedExeption('Invalid Attribute.');
 				}
