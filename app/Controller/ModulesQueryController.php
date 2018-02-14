b433b2a23d2ae7d91993a1e8f2a2871382cb786f
Juan C. Montes <33036804+juancmontes@users.noreply.github.com>
2018-02-14 11:03:44 +0100

    Update ModulesQueryController
    Support options (credentials) from config.

--- app/Controller/ModulesQueryController.php
+++ app/Controller/ModulesQueryController.php
@@ -11,13 +11,29 @@ class ModulesQueryController extends AppController {
 			'limit' => 60,
     );
     
-    public function query() {
+	public function query() {
         // Initialize models
+        $this->loadModel('Server');
 		$this->loadModel('Module');
 		$modules = $this->Module->getEnabledModules($this->Auth->user());
         
         // Prepare input
+        $url = Configure::read('Plugin.Enrichment_services_url') ? Configure::read('Plugin.Enrichment_services_url') : $this->Server->serverSettings['Plugin']['Enrichment_services_url']['value'];
+		$port = Configure::read('Plugin.Enrichment_services_port') ? Configure::read('Plugin.Enrichment_services_port') : $this->Server->serverSettings['Plugin']['Enrichment_services_port']['value'];
         $data = $this->request->data;
+        $modname = $data['module'];
+        $options = array();
+        
+        // Support credentials
+        foreach ($modules['modules'] as $temp) {
+            if ($temp['name'] == $modname) {
+                if (isset($temp['meta']['config'])) {
+                    foreach ($temp['meta']['config'] as $conf) $options[$conf] = Configure::read('Plugin.Enrichment_' . $modname . '_' . $conf);
+                }
+                break;
+            }
+        }
+        if (!empty($options)) $data['config'] = $options;
         
         // Query
         $result = $this->Module->queryModuleServer('/query', json_encode($data), true);
