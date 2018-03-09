ad2165bb86ca7520593ce452482c300601aea981
chrisr3d <chris.studer.68@gmail.com>
2018-03-09 12:08:27 +0100

    First version parsing some attributes
    - More attribute types to be added
    - Objects to be parsed as well

--- app/files/scripts/stix_to_misp.py
+++ app/files/scripts/stix_to_misp.py
@@ -19,6 +19,8 @@ import sys, json, os, time
 import pymisp
 from stix.core import STIXPackage
 
+eventTypes = {"FileObjectType": "filename", "URIObjectType": "url", "WindowsRegistryKeyObjectType": "regkey"}
+
 descFilename = os.path.join(pymisp.__path__[0], 'data/describeTypes.json')
 with open(descFilename, 'r') as f:
     categories = json.loads(f.read())['result'].get('categories')
@@ -30,31 +32,32 @@ class StixParser():
     def loadEvent(self, args, pathname):
         try:
             filename = '{}/tmp/{}'.format(pathname, args[1])
+            if args[1].startswith('misp.'):
+                fromMISP = True
+            else:
+                fromMISP = False
             try:
                 with open(filename, 'r') as f:
                     self.event = json.loads(f.read())
                 self.isJson = True
             except:
                 event = STIXPackage.from_xml(filename)
-                if args[1].startswith('misp.'):
-                    fromMISP = True
-                else:
-                    fromMISP = False
                 self.isJson = False
                 if fromMISP:
                     self.event = event.related_packages.related_package[0].item.incidents[0]
                 else:
                     self.event = event
-                self.fromMISP = fromMISP
+            self.fromMISP = fromMISP
+            self.filename = filename
         except:
             print(json.dumps({'success': 0, 'message': 'The temporary STIX export file could not be read'}))
             sys.exit(0)
 
-    def handler(self, args):
+    def handler(self):
         if self.isJson:
-            self.namefile = args[1]
+            self.outputname = self.filename
         else:
-            self.namefile = '{}.json'.format(args[1])
+            self.outputname = '{}.json'.format(self.filename)
         if self.fromMISP:
             self.buildMispDict()
         else:
@@ -107,26 +110,54 @@ class StixParser():
     def parse_misp_indicator(self, indicator):
         if indicator.relationship in categories:
             self.parse_misp_attribute(indicator)
-        else:
-            self.parse_misp_object(indicator)
+        # else:
+        #     self.parse_misp_object(indicator)
 
     def parse_misp_attribute(self, indicator):
-        misp_attribute = pymisp.MISPAttribute()
-        misp_attribute.category = indicator.relationship
-        properties = indicator.item.observable
+        misp_attribute = {'category': str(indicator.relationship)}
+        item = indicator.item
+        misp_attribute['timestamp'] = self.getTimestampfromDate(item.timestamp)
+        properties = item.observable.object_.properties
+        attribute_type, attribute_value = self.handle_attribute_type(properties)
+        self.misp_event.add_attribute(attribute_type, attribute_value, **misp_attribute)
+
+    def handle_attribute_type(self, properties):
+        xsi_type = properties._XSI_TYPE
+        if xsi_type == 'EmailMessageObjectType':
+            return self.handle_email_message(properties)
+        elif xsi_type == 'FileObjectType':
+            return eventTypes[xsi_type], properties.file_name.value
+        elif xsi_type == 'URIObjectType':
+            return eventTypes[xsi_type], properties.value.value
+        elif xsi_type == 'WindowsRegistryKeyObjectType':
+            return eventTypes[xsi_type], properties.key.value
+        else:
+            print("Unparsed type: {}".format(xsi_type))
+            sys.exit(1)
+
+    def handle_email_message(self, properties):
+        if properties.from_:
+            return "email-src", properties.from_.address_value.value
+        elif properties.to:
+            return "email-dst", properties.to.value.address_value.value
 
     def parse_misp_object(self, indicator):
         name = str(indicator.relationship)
         if name in ['file']:
             misp_object = pymisp.MISPObject(name)
 
+    def saveFile(self):
+        eventDict = self.misp_event.to_json()
+        with open(self.outputname, 'w') as f:
+            f.write(eventDict)
 
 def main(args):
     pathname = os.path.dirname(args[0])
     stix_parser = StixParser()
     stix_parser.loadEvent(args, pathname)
-    stix_parser.handler(args)
-    print(stix_parser.misp_event)
+    stix_parser.handler()
+    stix_parser.saveFile()
+    print(1)
 
 if __name__ == "__main__":
     main(sys.argv)
