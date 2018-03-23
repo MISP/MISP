ddf481042f6fe7e04c9e6eb7339aa012333ffef8
iglocska <andras.iklody@gmail.com>
2018-03-23 09:40:02 +0100

    chg: Query string bumped

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller {
 
 	public $helpers = array('Utility', 'OrgImg');
 
-	private $__queryVersion = '31';
+	private $__queryVersion = '32';
 	public $pyMispVersion = '2.4.87';
 	public $phpmin = '5.6.5';
 	public $phprec = '7.0.16';
