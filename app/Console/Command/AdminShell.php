66a0dec11c92c793ef7f4a37e0974bbdd1d9aab3
iglocska <andras.iklody@gmail.com>
2018-04-01 17:31:31 +0200

    new: Added server setting management via the command line
    - Usage:
    
      - /var/www/MISP/app/Console/cake Admin getSetting [setting]
        - setting is optional, if none set "all" is assumed
        - returns all or a specific setting's current value and metadata
    
      - /var/www/MISP/app/Console/cake Admin setSetting [setting] [value]
        - set a given server setting by full setting name
        - for example the following will enable the import services:
          -  /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Import_services_enable" 1
    
    - This feature was created in support of the CIRCL global conglomerate's APAC HQ in Tokyo

--- app/Console/Command/AdminShell.php
+++ app/Console/Command/AdminShell.php
@@ -58,4 +58,30 @@ class AdminShell extends AppShell
 		$this->Job->saveField('message', 'Job done.');
 		$this->Job->saveField('status', 4);
 	}
+
+	public function getSetting() {
+		$param = empty($this->args[0]) ? 'all' : $this->args[0];
+		$settings = $this->Server->serverSettingsRead();
+		$result = $settings;
+		if (!empty($param)) {
+			$result = 'No valid setting found for ' . $param;
+			foreach ($settings as $setting) {
+				if ($setting['setting'] == $param) {
+					$result = $setting;
+					break;
+				}
+			}
+		}
+		echo json_encode($result, JSON_PRETTY_PRINT) . PHP_EOL;
+	}
+
+	public function setSetting() {
+		$setting = empty($this->args[0]) ? null : $this->args[0];
+		$value = empty($this->args[1]) ? null : $this->args[1];
+		if (empty($setting) || $value === null) {
+			echo 'Invalid parameters. Usage: ' . APP . 'Console/cake Admin setSetting [setting_name] [setting_value]';
+		} else {
+			$this->Server->serverSettingsSaveValue($setting, $value);
+		}
+	}
 }
