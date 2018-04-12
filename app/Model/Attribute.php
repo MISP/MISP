1cfa3385c6f072aeb49ec5fc46968254b7020a58
StefanKelm <StefanKelm@users.noreply.github.com>
2018-04-12 14:35:04 +0200

    Update Attribute.php
    According to https://cve.mitre.org/news/archives/2014/news.html#jan152014_New_CVE_ID_Format_in_Effect_as_of_January_1_2014 the four-fixed-digits requirement has been dropped

--- app/Model/Attribute.php
+++ app/Model/Attribute.php
@@ -1024,10 +1024,10 @@ class Attribute extends AppModel {
 				break;
 			case 'vulnerability':
 				$value = str_replace('–', '-', $value);
-				if (preg_match("#^(CVE-)[0-9]{4}(-)[0-9]{4,6}$#", $value)) {
+				if (preg_match("#^(CVE-)[0-9]{4}(-)[0-9]{4,}$#", $value)) {
 					$returnValue = true;
 				} else {
-					$returnValue = 'Invalid format. Expected: CVE-xxxx-xxxx.';
+					$returnValue = 'Invalid format. Expected: CVE-xxxx-xxxx...';
 				}
 				break;
 			case 'named pipe':
