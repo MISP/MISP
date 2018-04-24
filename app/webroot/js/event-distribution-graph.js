b2f39aa4112a63915397ecb988ed78e9c9aca4ee
Sami Mokaddem <sami.mokaddem@circl.lu>
2018-04-24 13:52:32 +0000

    center distribution graph inside the popover

--- app/webroot/js/event-distribution-graph.js
+++ app/webroot/js/event-distribution-graph.js
@@ -1,6 +1,7 @@
 var scope_id = $('#eventdistri_graph').data('event-id');
 var event_distribution = $('#eventdistri_graph').data('event-distribution');
 var extended_text = $('#eventdistri_graph').data('extended') == 1 ? true : false;
+var spanOffset = 15; // due to padding
 var payload = {};
 var distribution_chart;
 
@@ -63,11 +64,11 @@ function get_maximum_distribution(array) {
 function add_level_to_pb(distribution, maxLevel) {
 	var pb_container = document.getElementById('eventdistri_pb_container');
 	var pb = document.getElementById('eventdistri_pb_background');
-	var spanOffset = 15; // due to padding
+	document.getElementById('eventdistri_graph').style.left = spanOffset + 'px'; // center graph inside the popover
 	var pbStep = pb.clientWidth / 5.0;
+
 	// we get 2:connected_comm, 3:all_comm, 4:sharing_group
 	// we want 2:sharing_group, 3:connected_comm, 4:all_comm
-
 	distribution = jQuery.extend({}, distribution); // deep clone distribution object
 	distribution[0].num = 0;
 	distribution[1].num = 1;
@@ -122,7 +123,7 @@ $(document).ready(function() {
 		"title": "Atomic event distribution graph",
 		"html": true,
 		"content": function() { return $('#distribution_graph_container').html(); },
-		"template" : '<div class="popover" role="tooltip" style="z-index: 1;"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content" style="padding-left: 15px; padding-right: 30px;"></div></div>'
+		"template" : '<div class="popover" role="tooltip" style="z-index: 1;"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content" style="padding-left: '+spanOffset+'px; padding-right: '+spanOffset*2+'px;"></div></div>'
 	});
 
 	$('.distribution_graph').click(function() {
