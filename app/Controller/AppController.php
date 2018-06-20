381a4b8895f40ac4acc3365238d540a7eaa36b40
iglocska <andras.iklody@gmail.com>
2018-06-20 07:55:04 +0200

    fix: Don't require API users to acept the terms / change password to get going
    - to get the API key they need to log in anyway via the interface

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -329,11 +329,11 @@ class AppController extends Controller {
 		if ($this->Session->check(AuthComponent::$sessionKey)) {
 			if ($this->action !== 'checkIfLoggedIn' || $this->request->params['controller'] !== 'users') {
 				if (!empty(Configure::read('MISP.terms_file')) && !$this->Auth->user('termsaccepted') && (!in_array($this->request->here, array($base_dir.'/users/terms', $base_dir.'/users/logout', $base_dir.'/users/login', $base_dir.'/users/downloadTerms')))) {
-					if ($this->_isRest()) throw new MethodNotAllowedException('You have not accepted the terms of use yet, please log in via the web interface and accept them.');
-					$this->redirect(array('controller' => 'users', 'action' => 'terms', 'admin' => false));
+					//if ($this->_isRest()) throw new MethodNotAllowedException('You have not accepted the terms of use yet, please log in via the web interface and accept them.');
+					if (!$this->_isRest()) $this->redirect(array('controller' => 'users', 'action' => 'terms', 'admin' => false));
 				} else if ($this->Auth->user('change_pw') && (!in_array($this->request->here, array($base_dir.'/users/terms', $base_dir.'/users/change_pw', $base_dir.'/users/logout', $base_dir.'/users/login')))) {
-					if ($this->_isRest()) throw new MethodNotAllowedException('Your user account is expecting a password change, please log in via the web interface and change it before proceeding.');
-					$this->redirect(array('controller' => 'users', 'action' => 'change_pw', 'admin' => false));
+					//if ($this->_isRest()) throw new MethodNotAllowedException('Your user account is expecting a password change, please log in via the web interface and change it before proceeding.');
+					if (!$this->_isRest()) $this->redirect(array('controller' => 'users', 'action' => 'change_pw', 'admin' => false));
 				} else if (!$this->_isRest() && !($this->params['controller'] == 'news' && $this->params['action'] == 'index') && (!in_array($this->request->here, array($base_dir.'/users/terms', $base_dir.'/users/change_pw', $base_dir.'/users/logout', $base_dir.'/users/login')))) {
 					$newsread = $this->User->field('newsread', array('User.id' => $this->Auth->user('id')));
 					$this->loadModel('News');
