b321ef7a20c58e8e079fb2b70231f5e4f83016d7
jgo <jan-gerrit.goebel@t-systems.com>
2018-07-27 13:42:29 +0200

    attach found sightings to event item

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -2958,6 +2958,13 @@ class Event extends AppModel
                     }
                 }
             }
+            // zeroq: check if sightings are attached and add to event
+            if (isset($data['Sighting']) && !empty($data['Sighting'])) {
+                $this->Sighting = ClassRegistry::init('Sighting');
+                foreach ($data['Sighting'] as $s) {
+                    $result = $this->Sighting->saveSightings($s['attribute_uuid'], false, $s['date_sighting'], $user, $s['type'], $s['source']);
+                }
+            }
             if ($fromXml) {
                 $created_id = $this->id;
             }
@@ -3117,6 +3124,13 @@ class Event extends AppModel
                             ));
                         }
                     }
+                }
+            }
+            // zeroq: if sightings then attach to event
+            if (isset($data['Sighting']) && !empty($data['Sighting'])) {
+                $this->Sighting = ClassRegistry::init('Sighting');
+                foreach ($data['Sighting'] as $s) {
+                    $result = $this->Sighting->saveSightings($s['attribute_uuid'], false, $s['date_sighting'], $user, $s['type'], $s['source']);
                 }
             }
             // if published -> do the actual publishing
