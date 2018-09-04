f378e521f1f68e0afb7450d9363afb953c36a309
chrisr3d <chris.studer.68@gmail.com>
2018-09-04 11:16:25 +0200

    fix: [stix2 import] Keeping uuids from STIX objects imported as attributes

--- app/files/scripts/stix2/stix2misp.py
+++ app/files/scripts/stix2/stix2misp.py
@@ -237,9 +237,10 @@ class StixParser():
         self.misp_event.add_object(**misp_object)
 
     def parse_attribute(self, o, labels):
+        attribute_uuid = o['id'].split('--')[1]
         attribute_type = self.get_misp_type(labels)
         attribute_category = self.get_misp_category(labels)
-        attribute = {'type': attribute_type, 'category': attribute_category}
+        attribute = {'uuid': attribute_uuid, 'type': attribute_type, 'category': attribute_category}
         tags = [{'name': label} for label in labels[3:]]
         if tags:
             attribute['Tag'] = tags
