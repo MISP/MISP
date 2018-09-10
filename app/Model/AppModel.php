c680f0aff56771f47fc5b843683bec6e29ab8051
iglocska <andras.iklody@gmail.com>
2018-09-10 07:53:22 +0200

    fix: [cleanup] Cleanup of removed upgrade scripts

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -69,8 +69,7 @@ class AppModel extends Model
     public $db_changes = array(
         1 => false, 2 => false, 3 => false, 4 => true, 5 => false, 6 => false,
         7 => false, 8 => false, 9 => false, 10 => false, 11 => false, 12 => false,
-        13 => false, 14 => false, 15 => false, 16 => false, 17 => false, 18 => false,
-		19 => false, 20 => false
+        13 => false, 14 => false, 15 => false, 18 => false, 19 => false, 20 => false
     );
 
     public function afterSave($created, $options = array())
