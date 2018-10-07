808040547714bd1df9c8d1d3a57064fb209aeb85
Raphaël Vinot <raphael@vinot.info>
2018-10-07 22:53:37 +0200

    fix: Sort CSV file before comparing: we do not care what the order of the attributes is

--- tests/curl_tests.sh
+++ tests/curl_tests.sh
@@ -6,7 +6,7 @@ set -x
 AUTH="$1"
 
 curl -i -H "Accept: application/json" -H "content-type: application/json" -H "Authorization: $AUTH" --data "@event.json" -X POST http://misp.local/events
-curl -H "Authorization: $AUTH"  -X GET http://misp.local/events/csv/download/1 | sed -e 's/^M//g' | cut -d, -f2 --complement > 1.csv
+curl -H "Authorization: $AUTH"  -X GET http://misp.local/events/csv/download/1 | sed -e 's/^M//g' | cut -d, -f2 --complement | sort > 1.csv
 cat 1.csv
-cut -d, -f2 --complement event.csv > compare.csv
+cut -d, -f2 --complement event.csv | sort > compare.csv
 diff compare.csv 1.csv
