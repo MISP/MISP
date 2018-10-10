70f5c872cfea724198d80043bd624af6d2fffb6e
chrisr3d <chris.studer.68@gmail.com>
2018-10-10 10:59:17 +0200

    fix: [stix2 export] Fixed attributes data parsing
    - With json format, base64 & encode/decode
      operations are no longer needed since the base64
      string is already displayed in data

--- app/files/scripts/stix2/misp2stix2.py
+++ app/files/scripts/stix2/misp2stix2.py
@@ -429,7 +429,7 @@ class StixBuilder():
         killchain = self.create_killchain(category)
         labels = self.create_labels(attribute)
         attribute_value = attribute['value'] if attribute_type != "AS" else self.define_attribute_value(attribute['value'], attribute['comment'])
-        pattern = mispTypesMapping[attribute_type]['pattern'](attribute_type, attribute_value, b64encode(attribute['data'].getbuffer()).decode()[1:-1]) if attribute.get('data') else self.define_pattern(attribute_type, attribute_value)
+        pattern = mispTypesMapping[attribute_type]['pattern'](attribute_type, attribute_value, attribute['data']) if attribute.get('data') else self.define_pattern(attribute_type, attribute_value)
         indicator_args = {'id': indicator_id, 'type': 'indicator', 'labels': labels, 'kill_chain_phases': killchain,
                            'valid_from': self.misp_event['date'], 'created_by_ref': self.identity_id, 'pattern': pattern}
         if hasattr(attribute, 'Sighting'):
@@ -461,7 +461,7 @@ class StixBuilder():
         timestamp = self.get_datetime_from_timestamp(attribute['timestamp'])
         labels = self.create_labels(attribute)
         attribute_value = attribute['value'] if attribute_type != "AS" else self.define_attribute_value(attribute['value'], attribute['comment'])
-        observable = mispTypesMapping[attribute_type]['observable'](attribute_type, attribute_value, b64encode(attribute['data'].getbuffer())) if 'data' in attribute else self.define_observable(attribute_type, attribute_value)
+        observable = mispTypesMapping[attribute_type]['observable'](attribute_type, attribute_value, attribute['data']) if attribute.get('data') else self.define_observable(attribute_type, attribute_value)
         observed_data_args = {'id': observed_data_id, 'type': 'observed-data', 'number_observed': 1,
                               'first_observed': timestamp, 'last_observed': timestamp, 'labels': labels,
                               'created_by_ref': self.identity_id, 'objects': observable}
@@ -768,7 +768,7 @@ class StixBuilder():
             except:
                 mapping = "x_misp_{}_{}".format(attribute['type'], relation)
                 if relation in ('eml', 'screenshot'):
-                    message[mapping] = {'value': attribute_value, 'data': b64encode(attribute['data'].getbuffer()).decode()[1:-1]}
+                    message[mapping] = {'value': attribute_value, 'data': attribute['data']}
                 else:
                     message[mapping] = attribute_value
         if reply_to and 'additional_header_fields' in message:
@@ -796,7 +796,7 @@ class StixBuilder():
                 stix_type = "'x_misp_{}_{}'".format(attribute['type'], relation)
                 if relation in ('eml', 'screenshot'):
                     stix_type_data = "{}.data".format(stix_type)
-                    pattern += pattern_mapping.format(email_type, stix_type_data, b64encode(attribute['data'].getbuffer()).decode()[1:-1])
+                    pattern += pattern_mapping.format(email_type, stix_type_data, attribute['data'])
                     stix_type += ".value"
             pattern += pattern_mapping.format(email_type, stix_type, attribute['value'])
         return "[{}]".format(pattern[:-5])
@@ -816,7 +816,7 @@ class StixBuilder():
                 malware_sample['filename'] = filename
                 malware_sample['md5'] = md5
                 if attribute.get('data'):
-                    observable[str(n_object)] = {'type': 'artifact', 'payload_bin': b64encode(attribute['data'].getbuffer())}
+                    observable[str(n_object)] = {'type': 'artifact', 'payload_bin': attribute['data']}
                     observable_file['content_ref'] = str(n_object)
                     n_object += 1
             elif attribute_type in ('filename', 'md5'):
@@ -849,7 +849,7 @@ class StixBuilder():
                 malware_sample['filename'] = filename
                 malware_sample['md5'] = md5
                 if attribute.get('data'):
-                    pattern += "{} AND ".format(attribute_data_pattern(b64encode(attribute['data'].getbuffer()).decode()))
+                    pattern += "{} AND ".format(attribute_data_pattern(attribute['data']))
             elif attribute_type in ("filename", "md5"):
                 d_pattern[attribute_type] = attribute['value']
             else:
