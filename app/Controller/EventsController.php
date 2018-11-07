a48897849cc0b0e9310c0b4ff0d0e7cd99174ea5
iglocska <andras.iklody@gmail.com>
2018-11-07 14:24:00 +0100

    fix: [API] throw a proper error when trying to edit an event without access to doing so

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -1910,8 +1910,13 @@ class EventsController extends AppController
         // check if private and user not authorised to edit
         if (!$this->_isSiteAdmin() && !($this->userRole['perm_sync'] && $this->_isRest())) {
             if (($this->Event->data['Event']['orgc_id'] != $this->_checkOrg()) || !($this->userRole['perm_modify'])) {
-                $this->Flash->error(__('You are not authorised to do that. Please consider using the \'propose attribute\' feature.'));
-                $this->redirect(array('controller' => 'events', 'action' => 'index'));
+				$message = __('You are not authorised to do that. Please consider using the \'propose attribute\' feature.');
+				if ($this->_isRest()) {
+					throw new MethodNotAllowedException($message);
+				} else {
+	                $this->Flash->error($message);
+	                $this->redirect(array('controller' => 'events', 'action' => 'index'));
+				}
             }
         }
         if (!$this->_isRest()) {
