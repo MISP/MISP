13993eb1c78e472a782996809903a6f8040b59b5
iglocska <andras.iklody@gmail.com>
2019-01-21 15:15:10 +0100

    new: [Tag collections] Export/import tag collections added

--- app/Model/TagCollection.php
+++ app/Model/TagCollection.php
@@ -82,7 +82,7 @@ class TagCollection extends AppModel
         if (isset($tagCollection['TagCollection'])) {
             $tagCollection = $tagCollection['TagCollection'];
         }
-        if (!empty($user['Role']['admin'])) {
+        if (!empty($user['Role']['perm_site_admin'])) {
             return true;
         }
         if (!$tagCollection['all_orgs'] && $user['org_id'] != $tagCollection['org_id']) {
@@ -118,5 +118,68 @@ class TagCollection extends AppModel
             }
         }
         return $single ? $tagCollections[0] : $tagCollections;
+    }
+
+    public function import($collections, $user)
+    {
+        $results = array('successes' => 0, 'fails' => 0);
+        if (empty($collections)) {
+            return $results;
+        }
+        if (isset($collections['TagCollection'])) {
+            $collections = array($collections);
+        }
+        foreach ($collections as $collection) {
+            $existingCollection = $this->find('first', array(
+                'conditions' => array(
+                    'TagCollection.uuid' => $collection['TagCollection']['uuid']
+                ),
+                'recursive' => -1
+            ));
+            if (!empty($existingCollection)) {
+                continue;
+            }
+            if (isset($collection['TagCollection']['id'])) {
+                unset($collection['TagCollection']['id']);
+            }
+            $this->create();
+            $collection['TagCollection']['user_id'] = $user['id'];
+            $collection['TagCollection']['org_id'] = $user['org_id'];
+            if (!$this->save($collection)) {
+                $results['fails']++;
+            } else {
+                $results['successes']++;
+                if (!empty($collection['TagCollectionTag'])) {
+                    foreach ($collection['TagCollectionTag'] as $tct) {
+                        $tag_id = $this->TagCollectionTag->Tag->captureTag($tct['Tag'], $user);
+                        $this->TagCollectionTag->create();
+                        $this->TagCollectionTag->save(array(
+                            'tag_collection_id' => $this->id,
+                            'tag_id' => $tag_id
+                        ));
+                    }
+                }
+                if (!empty($collection['Galaxy'])) {
+                    foreach ($collection['Galaxy'] as $galaxy) {
+                        foreach ($galaxy['GalaxyCluster'] as $cluster) {
+                            debug($galaxy);
+                            $tag_id = $this->TagCollectionTag->Tag->lookupTagIdFromName($cluster['tag_name']);
+                            if ($tag_id === -1) {
+                                $tag = array(
+                                    'name' => $cluster['tag_name']
+                                );
+                                $tag_id = $this->TagCollectionTag->Tag->captureTag($tag, $user);
+                            }
+                            $this->TagCollectionTag->create();
+                            $this->TagCollectionTag->save(array(
+                                'tag_collection_id' => $this->id,
+                                'tag_id' => $tag_id
+                            ));
+                        }
+                    }
+                }
+            }
+        }
+        return $results;
     }
 }
