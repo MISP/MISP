b64d5c82a74a3c6780c954faa19c90b8554e8dd3
mokaddem <sami.mokaddem@circl.lu>
2019-01-25 09:34:55 +0100

    fix: [attribute] Prevent undefined index on tag filtering.
    As tags are popped from the attribute scope first, they will not be
    available in the event scope.

--- app/Model/Attribute.php
+++ app/Model/Attribute.php
@@ -3796,7 +3796,7 @@ class Attribute extends AppModel
             );
             foreach ($params as $param => $paramData) {
                 foreach ($simple_params as $scope => $simple_param_scoped) {
-                    if (isset($simple_param_scoped[$param]) && $params[$param] !== false) {
+                    if (isset($simple_param_scoped[$param]) && isset($params[$param]) && $params[$param] !== false) {
                         $options = array(
                             'filter' => $param,
                             'scope' => $scope,
