66037a36c55c66d4d2fe41f71619bc79e27dfdc5
iglocska <andras.iklody@gmail.com>
2019-01-31 11:49:15 +0100

    fix: [API] Use restresponse to view an added event via /events/add

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -1655,13 +1655,10 @@ class EventsController extends AppController
                         }
                         // REST users want to see the newly created event
                         $results = $this->Event->fetchEvent($this->Auth->user(), array('eventid' => $created_id));
-                        $event = $results[0];
                         if (!empty($validationErrors)) {
-                            $event['errors'] = $validationErrors;
+                            $results[0]['errors'] = $validationErrors;
                         }
-                        $this->set('event', $event);
-                        $this->render('view');
-                        return true;
+                        return $this->RestResponse->viewData($results[0], $this->response->type());
                     } else {
                         // redirect to the view of the newly created event
                         $this->Flash->success(__('The event has been saved'));
