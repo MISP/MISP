f039f02e6da5bb0497e8891eb996c825ec191f53
iglocska <andras.iklody@gmail.com>
2019-02-18 14:37:42 +0100

    fix: [SMIME] Formating issues resolved

--- app/View/Helper/UtilityHelper.php
+++ app/View/Helper/UtilityHelper.php
@@ -3,8 +3,9 @@ App::uses('AppHelper', 'View/Helper');
 
     class UtilityHelper extends AppHelper {
         public function space2nbsp($string) {
-            $string = str_replace("\t", "    ", $string);
-            $string = str_replace(" ", "&nbsp", $string);
+            $string = str_replace("\t", "&nbsp&nbsp&nbsp&nbsp", $string);
+            $string = preg_replace("/\s\s+/", "&nbsp;", $string);
+            //$string = str_replace(' ', "&nbsp", $string);
             return $string;
         }
     }
