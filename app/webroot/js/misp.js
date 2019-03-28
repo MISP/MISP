01b7a24d024d20dea8690b93fd76ba287548e232
mokaddem <sami.mokaddem@circl.lu>
2019-03-28 15:35:10 +0100

    fix: [picturePreview] Don't show the fullscreen button if php-gd is not installed.

--- app/webroot/js/misp.js
+++ app/webroot/js/misp.js
@@ -84,7 +84,9 @@ function screenshotPopup(url, title) {
     popupHtml = '<it class="fa fa-spin fa-spinner" style="font-size: xx-large; color: white; position: fixed; left: 50%; top: 50%;"></it>'
     popupHtml += '<img class="screenshot_box-content hidden" src="' + url + '" id="screenshot-image" title="' + title + '" alt="' + title + '" onload="$(this).show(); $(this).parent().find(\'.fa-spinner\').remove();"/>';
     popupHtml += '<div class="close-icon useCursorPointer" onClick="closeScreenshot();"></div>';
-    popupHtml += '<a class="close-icon useCursorPointer fa fa-expand" style="right: 20px; background: black; color: white; text-decoration: none;" target="_blank" href="' + url + '" ></a>';
+    if (!url.startsWith('data:image/')) {
+        popupHtml += '<a class="close-icon useCursorPointer fa fa-expand" style="right: 20px; background: black; color: white; text-decoration: none;" target="_blank" href="' + url + '" ></a>';
+    }
     popupHtml += '<div style="height: 20px;"></div>'; // see bottom of image for large one
     $('#screenshot_box').html(popupHtml);
     $('#screenshot_box').css({
