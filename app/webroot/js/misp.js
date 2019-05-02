8e5c6e7192dc206d063f4f35c13914bbad9c99e9
mokaddem <sami.mokaddem@circl.lu>
2019-05-02 11:21:58 +0200

    chg: [diagnostic] Changed update button with more relevant icons

--- app/webroot/js/misp.js
+++ app/webroot/js/misp.js
@@ -3753,7 +3753,8 @@ function submitSubmoduleUpdate(clicked) {
     var submodule_path = $clicked.data('submodule');
     $.ajax({
         beforeSend: function (XMLHttpRequest) {
-            $clicked.addClass('fa-spin');
+            $clicked.removeClass('fa-download');
+            $clicked.addClass('fa-spin fa-spinner');
         },
         dataType:"html",
         cache: false,
@@ -3780,7 +3781,8 @@ function submitSubmoduleUpdate(clicked) {
                     $('#submoduleGitResult').removeClass('green').addClass('red').text(data.output);
                 },
                 complete:function() {
-                    $clicked.removeClass('fa-spin');
+                    $clicked.removeClass('fa-spin fa-spinner');
+                    $clicked.addClass('fa-download');
                     $form.remove();
                 },
                 type:"post",
