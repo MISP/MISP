ef4c99d964ae249d6071c444e829820d9d2a20cd
Steve Clement <steve@localhost.lu>
2019-05-07 11:31:26 +0900

    fix: [AdminShell] Misplaced the debug message.

--- app/Console/Command/AdminShell.php
+++ app/Console/Command/AdminShell.php
@@ -311,11 +311,10 @@ class AdminShell extends AppShell
         $whoami = exec('whoami');
         if ($whoami === 'httpd' || $whoami === 'www-data' || $whoami === 'apache') {
             echo 'Executing all updates to bring the database up to date with the current version.' . PHP_EOL;
-            echo 'You tried to run this command as: ' . $whoami . PHP_EOL;
             $this->Server->runUpdates(true);
             echo 'All updates completed.' . PHP_EOL;
         } else {
-            die('This OS user is not allowed to run this command.'. PHP_EOL. 'Run it under `www-data` or `httpd`.' . PHP_EOL);
+            die('This OS user is not allowed to run this command.'. PHP_EOL. 'Run it under `www-data` or `httpd`.' . PHP_EOL . 'You tried to run this command as: ' . $whoami . PHP_EOL;
         }
     }
 
