80975bf7d3e922d128d92e4c3f2d9321c5be74dd
iglocska <andras.iklody@gmail.com>
2019-06-18 14:42:31 +0200

    fix: [UI] Enrichment hover should also work after sorting / changing the event attribute context, etc

--- app/webroot/js/misp.js
+++ app/webroot/js/misp.js
@@ -4015,32 +4015,6 @@ $(document).ready(function() {
             $('#quickFilterButton').trigger("click");
         }
     });
-    $(".eventViewAttributeHover").mouseenter(function() {
-        $('#' + currentPopover).popover('destroy');
-        var type = $(this).attr('data-object-type');
-        var id = $(this).attr('data-object-id');
-
-        if (type + "_" + id in ajaxResults["hover"]) {
-            var element = $('#' + type + '_' + id + '_container');
-            element.popover({
-                title: attributeHoverTitle(id, type),
-                content: ajaxResults["hover"][type + "_" + id],
-                placement: attributeHoverPlacement(element),
-                html: true,
-                trigger: 'manual',
-                container: 'body'
-            }).popover('show');
-            currentPopover = type + '_' + id + '_container';
-        } else {
-          timer = setTimeout(function () {
-              runHoverLookup(type, id)
-            },
-            500
-          );
-        }
-    }).mouseout(function() {
-        clearTimeout(timer);
-    });
     $(".queryPopover").click(function() {
         url = $(this).data('url');
         id = $(this).data('id');
