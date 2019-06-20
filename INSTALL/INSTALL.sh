08e7ca34b84fdbade732d42dc4883f7ebaf991a6
Steve Clement <steve@localhost.lu>
2019-06-20 14:00:42 +0900

    chg: [installer] Update installer to Fix Auto-VMs

--- INSTALL/INSTALL.sh
+++ INSTALL/INSTALL.sh
@@ -967,7 +967,16 @@ aptUpgrade () {
   debug "Upgrading system"
   checkAptLock
   sudo apt-get update
-  sudo apt-get upgrade -qy
+
+  # If we run in non-interactive mode, make sure we do not stop all of a sudden
+  if [[ "${PACKER}" == "1" || "${UNATTENDED}" == "1" ]]; then
+    export DEBIAN_FRONTEND=noninteractive
+    export DEBIAN_PRIORITY=critical
+    sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+    sudo -E apt-get -qy autoclean
+  else
+    sudo apt-get upgrade -qy
+  fi
 }
 
 # check if sudo is installed
