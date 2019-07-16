458bd23937a98674007415ba1f5bef6c7c44d282
mokaddem <sami.mokaddem@circl.lu>
2019-07-16 16:08:28 +0200

    chg: bumped queryversion

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller
 
     public $helpers = array('Utility', 'OrgImg', 'FontAwesome');
 
-    private $__queryVersion = '80';
+    private $__queryVersion = '81';
     public $pyMispVersion = '2.4.111';
     public $phpmin = '7.0';
     public $phprec = '7.2';
