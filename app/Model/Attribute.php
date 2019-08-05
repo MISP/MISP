d7849035585e6b41ff4323576e04e7252d671c55
iglocska <andras.iklody@gmail.com>
2019-08-05 10:47:26 +0200

    new: [internal] Default field list added for attributes
    - let's try to standardised on things we output instead of doing it manually. It's a first step

--- app/Model/Attribute.php
+++ app/Model/Attribute.php
@@ -34,6 +34,10 @@ class Attribute extends AppModel
             'distribution' => array('desc' => 'Describes who will have access to the event.')
     );
 
+    public $defaultFields = array(
+        'id', 'event_id', 'object_id', 'object_relation', 'category', 'type', 'value', 'to_ids', 'uuid', 'timestamp', 'distribution', 'sharing_group_id', 'comment', 'deleted', 'disable_correlation'
+    );
+
     public $distributionDescriptions = array(
         0 => array('desc' => 'This field determines the current distribution of the event', 'formdesc' => "This setting will only allow members of your organisation on this server to see it."),
         1 => array('desc' => 'This field determines the current distribution of the event', 'formdesc' => "Organisations that are part of this MISP community will be able to see the event."),
