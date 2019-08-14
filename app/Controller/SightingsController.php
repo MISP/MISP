74bf393eadef1545e033f01669bca74ffff9c8bb
iglocska <andras.iklody@gmail.com>
2019-08-14 11:43:14 +0200

    fix: [API] Delete sightings by UUID, fixes #4987

--- app/Controller/SightingsController.php
+++ app/Controller/SightingsController.php
@@ -228,7 +228,12 @@ class SightingsController extends AppController
         if (!$this->request->is('post')) {
             throw new MethodNotAllowedException('This action can only be accessed via a post request.');
         }
-        $sighting = $this->Sighting->find('first', array('conditions' => array('Sighting.id' => $id), 'recursive' => -1));
+        if (Validation::uuid($id)) {
+            $conditions = array('Sighting.uuid' => $id);
+        } else {
+            $conditions = array('Sighting.id' => $id);
+        }
+        $sighting = $this->Sighting->find('first', array('conditions' => $conditions, 'recursive' => -1));
         if (empty($sighting)) {
             throw new NotFoundException('Invalid sighting.');
         }
