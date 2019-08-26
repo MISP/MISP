1994750db11f5de084d4268314d8ad7010faf5fc
Pierre-Jean Grenier <zaphod@eleves.polytechnique.fr>
2019-08-26 17:59:12 +0200

    fix: Fix 'contain' param in app/Model/Attribute.php:fetchAttributes()
    When we specified eg. 'contain': array('Event'), the merge done by the function was incorrect, and only kept more restrictive stuff,
    while we wanted to get all the keys related to the Event.

--- app/Model/Attribute.php
+++ app/Model/Attribute.php
@@ -3077,6 +3077,14 @@ class Attribute extends AppModel
             );
         }
         if (isset($options['contain'])) {
+            // We may use a string instead of an array to ask for everything
+            // instead of some specific attributes. If so, remove the array from
+            // params, as we will later add the string.
+            foreach($options['contain'] as $contain) {
+                if (gettype($contain) == "string" && isset($params['contain'][$contain])) {
+                    unset($params['contain'][$contain]);
+                }
+            }
             $params['contain'] = array_merge_recursive($params['contain'], $options['contain']);
         }
         if (isset($options['page'])) {
