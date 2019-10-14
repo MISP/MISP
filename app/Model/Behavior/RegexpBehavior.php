4260216ad893db7375d7282af189647db8bac5b7
Jakub Onderka <jakub.onderka@gmail.com>
2019-10-14 20:17:14 +0200

    fix: [internal] Load Regexp just when they are requested

--- app/Model/Behavior/RegexpBehavior.php
+++ app/Model/Behavior/RegexpBehavior.php
@@ -4,37 +4,40 @@ App::uses('Regexp', 'Model');
 
 /**
  * Behavior to regexp all string fields in a model
- *
  */
 class RegexpBehavior extends ModelBehavior
 {
-    private $__allRegexp = array();
+    private $__allRegexp = null;
 
     public $excluded_types = array('sigma', 'float');
-
-    public function setup(Model $model, $config = null)
-    {
-        $regexp = new Regexp();
-        $this->__allRegexp = $regexp->find('all', array('order' => 'id ASC'));
-    }
 
     /**
      * replace the current value according to the regexp rules, or block blacklisted regular expressions
      *
      * @param Model $Model
-     * @param unknown_type $array
+     * @param string $type
+     * @param string $value
+     * @return string
      */
     public function runRegexp(Model $Model, $type, $value)
     {
         if (in_array($type, $this->excluded_types)) {
             return $value;
         }
+
+        if ($this->__allRegexp === null) {
+            $regexp = new Regexp();
+            $this->__allRegexp = $regexp->find('all', array('order' => 'id ASC'));
+        }
+
         foreach ($this->__allRegexp as $regexp) {
-            if (!empty($regexp['Regexp']['replacement']) && !empty($regexp['Regexp']['regexp']) && ($regexp['Regexp']['type'] === 'ALL' || $regexp['Regexp']['type'] === $type)) {
-                $value = preg_replace($regexp['Regexp']['regexp'], $regexp['Regexp']['replacement'], $value);
-            }
-            if (empty($regexp['Regexp']['replacement']) && preg_match($regexp['Regexp']['regexp'], $value) && ($regexp['Regexp']['type'] === 'ALL' || $regexp['Regexp']['type'] === $type)) {
-                return false;
+            if ($regexp['Regexp']['type'] === 'ALL' || $regexp['Regexp']['type'] === $type) {
+                if (!empty($regexp['Regexp']['replacement']) && !empty($regexp['Regexp']['regexp'])) {
+                    $value = preg_replace($regexp['Regexp']['regexp'], $regexp['Regexp']['replacement'], $value);
+                }
+                if (empty($regexp['Regexp']['replacement']) && preg_match($regexp['Regexp']['regexp'], $value)) {
+                    return false;
+                }
             }
         }
         return $value;
