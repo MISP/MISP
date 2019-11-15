35c739980e939febc204fb4d2ce28dabbdd0890a
Steve Clement <steve@localhost.lu>
2019-11-15 10:13:49 +0900

    chg: [REST] Updated to ExpandedPyMISP

--- app/Controller/ServersController.php
+++ app/Controller/ServersController.php
@@ -1941,9 +1941,9 @@ misp_verifycert = %s
 relative_path = \'%s\'
 body = %s
 
-from pymisp import PyMISP
+from pymisp import ExpandedPyMISP
 
-misp = PyMISP(misp_url, misp_key, misp_verifycert)
+misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
 misp.direct_call(relative_path, body)
 ',
             $baseurl,
