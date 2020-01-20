24d13b769c85a870ce17360b06b24fa0ad7cb21e
iglocska <andras.iklody@gmail.com>
2020-01-20 10:18:00 +0100

    fix: [update] typo fixed

--- app/Model/AdminSetting.php
+++ app/Model/AdminSetting.php
@@ -21,7 +21,7 @@ class AdminSetting extends AppModel
         $setting_object = $this->find('first', array(
             'conditions' => array('setting' => $setting)
         ));
-        $this->deleteAll('setting' => $setting);
+        $this->deleteAll(array('setting' => $setting));
         $this->create();
         $setting_object['AdminSetting'] = array('setting' => $setting, 'value' => $value);
         if ($this->save($setting_object)) {
