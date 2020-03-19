4633d2541803ffd2e01068151b0742970d99d7dd
mokaddem <sami.mokaddem@circl.lu>
2020-03-19 14:39:52 +0100

    new: [helper:scopedCSS] Moved implementation in a helper

--- app/View/Dashboards/widget_loader.ctp
+++ app/View/Dashboards/widget_loader.ctp
@@ -1,42 +1,9 @@
 <?php
-    function endsWith($haystack, $needle)
-    {
-        $length = strlen($needle);
-        if ($length == 0) {
-            return true;
-        }
-        return (substr($haystack, -$length) === $needle);
-    }
-    function preppendScopedId($widgetCSS, $seed)
-    {
-        $prependSelector = sprintf('[data-scoped="%s"]', $seed);
-        $cssLines = explode("\n", $widgetCSS);
-        foreach ($cssLines as $i => $line) {
-            if (strlen($line) > 0) {
-                if (endsWith($line, "{")) {
-                    $cssLines[$i] = sprintf("%s %s", $prependSelector, $line);
-                }
-            }
-        }
-        $cssScopedLines = implode(PHP_EOL, $cssLines);
-        return sprintf("<style>%s%s%s</style>", PHP_EOL, $cssScopedLines, PHP_EOL);
-    }
-
     $widgetHtml = $this->element('/dashboard/Widgets/' . $config['render']);
-    $widgetCSS = "";
-    $seed = "";
-    $styleTag = "<style scoped>";
-    $styleClosingTag = "</style>";
-    $styleTagIndex = strpos($widgetHtml, $styleTag);
-    $closingStyleTagIndex = strpos($widgetHtml, $styleClosingTag) + strlen($styleClosingTag);
-    if ($styleTagIndex !== false && $closingStyleTagIndex !== false && $closingStyleTagIndex > $styleTagIndex) { // enforced scoped css
-        $seed = rand();
-        $widgetCSS = substr($widgetHtml, $styleTagIndex, $closingStyleTagIndex);
-        $widgetHtml = str_replace($widgetCSS, "", $widgetHtml); // remove CSS
-        $widgetCSS = str_replace($styleTag, "", $widgetCSS);    // remove the style node
-        $widgetCSS = str_replace($styleClosingTag, "", $widgetCSS); // remove closing style node
-        $widgetCSS = preppendScopedId($widgetCSS, $seed);
-    }
+    $result = $this->ScopedCSS->createScopedCSS($widgetHtml);
+    $seed = $result['seed'];
+    $widgetHtml = $result['html'];
+    $widgetCSS = $result['css'];
 ?>
 <div id="widgetContentInner_<?= h($widget_id) ?>" <?php echo !empty($seed) ? sprintf("data-scoped=\"%s\" ", $seed) : "" ?>>
     <?php
---
+++ app/View/Helper/ScopedCSSHelper.php
@@ -0,0 +1,54 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+
+    // prepend user names on the header with some text based on the given rules
+    class ScopedCSSHelper extends AppHelper {
+
+        private function endsWith($haystack, $needle)
+        {
+            $length = strlen($needle);
+            if ($length == 0) {
+                return true;
+            }
+            return (substr($haystack, -$length) === $needle);
+        }
+
+        private function preppendScopedId($css, $seed)
+        {
+            $prependSelector = sprintf('[data-scoped="%s"]', $seed);
+            $cssLines = explode("\n", $css);
+            foreach ($cssLines as $i => $line) {
+                if (strlen($line) > 0) {
+                    if (endsWith($line, "{")) {
+                        $cssLines[$i] = sprintf("%s %s", $prependSelector, $line);
+                    }
+                }
+            }
+            $cssScopedLines = implode(PHP_EOL, $cssLines);
+            return sprintf("<style>%s%s%s</style>", PHP_EOL, $cssScopedLines, PHP_EOL);
+        }
+
+        public function createScopedCSS($html)
+        {
+            $css = "";
+            $seed = "";
+            $htmlStyleTag = "<style scoped>";
+            $styleClosingTag = "</style>";
+            $styleTagIndex = strpos($html, $htmlStyleTag);
+            $closingStyleTagIndex = strpos($html, $styleClosingTag) + strlen($styleClosingTag);
+            if ($styleTagIndex !== false && $closingStyleTagIndex !== false && $closingStyleTagIndex > $styleTagIndex) { // enforced scoped css
+                $seed = rand();
+                $css = substr($html, $styleTagIndex, $closingStyleTagIndex);
+                $html = str_replace($css, "", $html);           // remove CSS part
+                $css = str_replace($htmlStyleTag, "", $css);    // remove the style node
+                $css = str_replace($styleClosingTag, "", $css); // remove closing style node
+                $css = preppendScopedId($css, $seed);
+            }
+            return array(
+                "seed" => $seed,
+                "html" => $html,
+                "css" => $css
+            );
+        }
+
+    }
