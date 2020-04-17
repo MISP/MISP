078bf123a1e91cbe68a8113b7da64d51b1755816
iglocska <andras.iklody@gmail.com>
2020-04-17 14:23:34 +0200

    chg: [ACL] added the feed data reload

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -46,7 +46,7 @@ class AppController extends Controller
 
     public $helpers = array('Utility', 'OrgImg', 'FontAwesome', 'UserName', 'DataPathCollector');
 
-    private $__queryVersion = '102';
+    private $__queryVersion = '103';
     public $pyMispVersion = '2.4.123';
     public $phpmin = '7.2';
     public $phprec = '7.4';
