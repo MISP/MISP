d0ff0f66f5fe8c2768922bacea985d14d2d8a69b
chrisr3d <chris.studer.68@gmail.com>
2020-05-19 13:51:42 +0200

    fix: [stix2 export] Removed object attributes added for test purposes

--- app/files/scripts/stix2/misp2stix2.py
+++ app/files/scripts/stix2/misp2stix2.py
@@ -1020,7 +1020,6 @@ class StixBuilder():
         file_observable['type'] = 'file'
         n_object = 0
         attributes_dict = self.create_file_attributes_dict(attributes, object_id)
-        attributes_dict.update({'path': ['/home/chrisr3d/git/'], 'fullpath': ['/home/chrisr3d/git/MISP/cleanMISP/app/files/scripts/stix2']})
         for key, feature in fileMapping.items():
             if key in attributes_dict:
                 if key in hash_types:
