857d0dffa3bbfd0c151fa42fe6346e707e14e337
chrisr3d <chris.studer.68@gmail.com>
2020-05-19 23:55:39 +0200

    wip: [stix2 import] Moving small parsing functions to the main script
    - Also passing the function names only instead of
      storing functions themselves in the dictionary

--- app/files/scripts/stix2/stix2misp_mapping.py
+++ app/files/scripts/stix2/stix2misp_mapping.py
@@ -1,121 +1,61 @@
-def define_observable_hash_type(h_type):
-    if 'sha' in h_type:
-        return 'SHA-{}'.format(h_type.split('sha')[1])
-    if h_type == 'md5':
-        return h_type.upper()
-    return h_type
+attributes_mapping = {
+    'filename': '_parse_name',
+    'ip-src': '_parse_value',
+    'ip-dst': '_parse_value',
+    'hostname': '_parse_value',
+    'domain': '_parse_value',
+    'domain|ip': '_parse_domain_ip_attribute',
+    'email-src': '_parse_value',
+    'email-dst': '_parse_value',
+    'email-attachment': '_parse_name',
+    'url': '_parse_value',
+    'regkey': '_parse_regkey_attribute',
+    'regkey|value': '_parse_regkey_value',
+    'malware-sample': '_parse_malware_sample',
+    'mutex': '_parse_name',
+    'uri': '_parse_value',
+    'port': '_parse_port',
+    'ip-dst|port': '_parse_ip_port',
+    'ip-src|port': '_parse_ip_port',
+    'hostname|port': '_parse_hostname_port',
+    'email-reply-to': '_parse_value',
+    'attachment': '_parse_attachment',
+    'mac-address': '_parse_value',
+    'AS': '_parse_number'
+}
 
-def parse_name(observable, _):
-    return observable['0'].get('name')
-
-def parse_value(observable, _):
-    return observable['0'].get('value')
-
-def parse_attachment(observable, _):
-    if len(observable) > 1:
-        return observable['1'].get('name'), observable['0'].get('payload_bin')
-    return observable['0'].get('name')
-
-def parse_domain_ip(observable, _):
-    return "{}|{}".format(parse_value(observable, _), observable['1'].get('value'))
-
-def parse_email_message(observable, attribute_type):
-    return observable['0'].get(attribute_type.split('-')[1])
-
-def parse_hash(observable, attribute_type):
-    observable_type = define_observable_hash_type(attribute_type)
-    return observable['0']['hashes'].get(observable_type)
-
-def parse_ip_port(observable, _):
-    try:
-        port = observable['1']['src_port']
-    except KeyError:
-        port = observable['1']['dst_port']
-    return '{}|{}'.format(parse_value(observable, _), port)
-
-def parse_hostname_port(observable, _):
-    return '{}|{}'.format(parse_value(observable, _), observable['1'].get('dst_port'))
-
-def parse_filename_hash(observable, attribute_type):
-    _, h = attribute_type.split('|')
-    return "{}|{}".format(parse_name(observable, _), parse_hash(observable, h))
-
-def parse_malware_sample(observable, _):
-    if len(observable) > 1:
-        file_observable = observable['1']
-        filename = file_observable['name']
-        md5 = file_observable['hashes']['MD5']
-        return "{}|{}".format(filename, md5), observable['0'].get('payload_bin')
-    return parse_filename_hash(observable, 'filename|md5')
-
-def parse_number(observable, _):
-    return observable['0'].get('number')
-
-def parse_port(observable, _):
-    return observable
-
-def parse_regkey(observable, _):
-    return observable['0'].get('key')
-
-def parse_regkey_value(observable, _):
-    return '{}|{}'.format(parse_regkey(observable,_), parse_name(observable, _))
-
-misp_types_mapping = {
-    'md5': parse_hash,
-    'sha1': parse_hash,
-    'sha256': parse_hash,
-    'filename': parse_name,
-    'filename|md5': parse_filename_hash,
-    'filename|sha1': parse_filename_hash,
-    'filename|sha256': parse_filename_hash,
-    'ip-src': parse_value,
-    'ip-dst': parse_value,
-    'hostname': parse_value,
-    'domain': parse_value,
-    'domain|ip': parse_domain_ip,
-    'email-src': parse_value,
-    'email-dst': parse_value,
-    'email-subject': parse_email_message,
-    'email-body': parse_email_message,
-    'email-attachment': parse_name,
-    'url': parse_value,
-    'regkey': parse_regkey,
-    'regkey|value': parse_regkey_value,
-    'malware-sample': parse_malware_sample,
-    'mutex': parse_name,
-    'uri': parse_value,
-    'authentihash': parse_hash,
-    'ssdeep': parse_hash,
-    'imphash': parse_hash,
-    'pehash': parse_hash,
-    'impfuzzy': parse_hash,
-    'sha224': parse_hash,
-    'sha384': parse_hash,
-    'sha512': parse_hash,
-    'sha512/224': parse_hash,
-    'sha512/256': parse_hash,
-    'tlsh': parse_hash,
-    'cdhash': parse_hash,
-    'filename|authentihash': parse_filename_hash,
-    'filename|ssdeep': parse_filename_hash,
-    'filename|imphash': parse_filename_hash,
-    'filename|impfuzzy': parse_filename_hash,
-    'filename|pehash': parse_filename_hash,
-    'filename|sha224': parse_filename_hash,
-    'filename|sha384': parse_filename_hash,
-    'filename|sha512': parse_filename_hash,
-    'filename|sha512/224': parse_filename_hash,
-    'filename|sha512/256': parse_filename_hash,
-    'filename|tlsh': parse_filename_hash,
-    'x509-fingerprint-sha1': parse_hash,
-    'port': parse_port,
-    'ip-dst|port': parse_ip_port,
-    'ip-src|port': parse_ip_port,
-    'hostname|port': parse_hostname_port,
-    'email-reply-to': parse_value,
-    'attachment': parse_attachment,
-    'mac-address': parse_value,
-    'AS': parse_number
+attributes_type_mapping = {
+    'md5': '_parse_hash',
+    'sha1': '_parse_hash',
+    'sha256': '_parse_hash',
+    'filename|md5': '_parse_filename_hash',
+    'filename|sha1': '_parse_filename_hash',
+    'filename|sha256': '_parse_filename_hash',
+    'email-subject': '_parse_email_message',
+    'email-body': '_parse_email_message',
+    'authentihash': '_parse_hash',
+    'ssdeep': '_parse_hash',
+    'imphash': '_parse_hash',
+    'pehash': '_parse_hash',
+    'impfuzzy': '_parse_hash',
+    'sha224': '_parse_hash',
+    'sha384': '_parse_hash',
+    'sha512': '_parse_hash',
+    'sha512/224': '_parse_hash',
+    'sha512/256': '_parse_hash',
+    'tlsh': '_parse_hash',
+    'cdhash': '_parse_hash',
+    'filename|authentihash': '_parse_filename_hash',
+    'filename|ssdeep': '_parse_filename_hash',
+    'filename|imphash': '_parse_filename_hash',
+    'filename|impfuzzy': '_parse_filename_hash',
+    'filename|pehash': '_parse_filename_hash',
+    'filename|sha224': '_parse_filename_hash',
+    'filename|sha384': '_parse_filename_hash',
+    'filename|sha512': '_parse_filename_hash',
+    'filename|sha512/224': '_parse_filename_hash',
+    'filename|sha512/256': '_parse_filename_hash',
+    'filename|tlsh': '_parse_filename_hash'
 }
 
 address_family_attribute_mapping = {'type': 'text','object_relation': 'address-family'}
