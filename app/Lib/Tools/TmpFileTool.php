f46ca1634a2e566f662b19ea4338d1d702714c88
Jakub Onderka <jakub.onderka@gmail.com>
2020-06-20 09:54:32 +0200

    chg: [internal] Use tmp file fro Feed::getCache

--- app/Lib/Tools/TmpFileTool.php
+++ app/Lib/Tools/TmpFileTool.php
@@ -38,15 +38,33 @@ class TmpFileTool
     }
 
     /**
+     * @return Generator
+     * @throws Exception
+     */
+    public function lines()
+    {
+        $this->rewind();
+        while (!feof($this->tmpfile)) {
+            $result = fgets($this->tmpfile);
+            if ($result === false) {
+                throw new Exception('Could not read line from temporary file.');
+            }
+            yield $result;
+        }
+        fclose($this->tmpfile);
+        $this->tmpfile = null;
+    }
+
+    /**
      * @return string
      * @throws Exception
      */
     public function finish()
     {
-        fseek($this->tmpfile, 0);
+        $this->rewind();
         $final = stream_get_contents($this->tmpfile);
         if ($final === false) {
-            throw new Exception("Could not read from temporary file.");
+            throw new Exception('Could not read from temporary file.');
         }
         fclose($this->tmpfile);
         $this->tmpfile = null;
@@ -60,5 +78,17 @@ class TmpFileTool
     public function __toString()
     {
         return $this->finish();
+    }
+
+    /**
+     * Seek to start of file.
+     *
+     * @throws Exception
+     */
+    private function rewind()
+    {
+        if (fseek($this->tmpfile, 0) === -1) {
+            throw new Exception('Could not seek to start of temporary file.');
+        }
     }
 }
