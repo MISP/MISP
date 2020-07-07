f7a3ed1db8e9e656beeb6b74504c5b09a6537d69
mokaddem <sami.mokaddem@circl.lu>
2020-07-07 08:12:12 +0200

    fix: [appModel:db_changes] Added new entry

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -81,7 +81,7 @@ class AppModel extends Model
         33 => false, 34 => false, 35 => false, 36 => false, 37 => false, 38 => false,
         39 => false, 40 => false, 41 => false, 42 => false, 43 => false, 44 => false,
         45 => false, 46 => false, 47 => false, 48 => false, 49 => false, 50 => false,
-        51 => false, 52 => false, 53 => false, 54 => false, 55 => false,
+        51 => false, 52 => false, 53 => false, 54 => false, 55 => false, 56 => false,
     );
 
     public $advanced_updates_description = array(
