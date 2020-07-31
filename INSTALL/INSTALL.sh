29e3d38186ec90eefb8f18626b3cfc696c2fe075
Steven Goossens <steven@teamg.be>
2020-07-31 15:52:34 +0200

    Manually run commands for Mysql hardening

--- INSTALL/INSTALL.sh
+++ INSTALL/INSTALL.sh
@@ -1282,7 +1282,16 @@ prepareDB () {
     else
       pw=${MISP_PASSWORD}
     fi
-    echo -e "\ny\n${DBPASSWORD_ADMIN}\n${DBPASSWORD_ADMIN}\ny\ny\ny\ny\n " | sudo mysql_secure_installation 2>/dev/null
+    # Make sure that NOBODY can access the server without a password
+	mysql -e "UPDATE mysql.user SET Password = PASSWORD('${DBPASSWORD_ADMIN}') WHERE User = 'root'"
+	# Kill the anonymous users
+	mysql -e "DROP USER ''@'localhost'"
+	# Because our hostname varies we'll use some Bash magic here.
+	mysql -e "DROP USER ''@'$(hostname)'"
+	# Kill off the demo database
+	mysql -e "DROP DATABASE test"
+	# Make our changes take effect
+	mysql -e "FLUSH PRIVILEGES"
     sudo apt-get purge -y expect ; sudo apt autoremove -qy
   fi 
 
