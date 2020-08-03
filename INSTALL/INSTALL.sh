4152760565cb3936387022873a24c8e7d2eb9779
Steven Goossens <steven@teamg.be>
2020-08-03 13:40:54 +0200

    remove explicit service start

--- INSTALL/INSTALL.sh
+++ INSTALL/INSTALL.sh
@@ -1272,7 +1272,6 @@ prepareDB () {
   if [[ ! -e /var/lib/mysql/misp/users.ibd ]]; then
     #Make sure initial tables are created in MySQL
     sudo mysql_install_db
-    sudo mysqld
     debug "Setting up database"
 
     # FIXME: If user 'misp' exists, and has a different password, the below WILL fail.
