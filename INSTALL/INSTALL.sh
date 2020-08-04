bdadb99e93755927148e4637851d434204e48ca3
Steven Goossens <steven@teamg.be>
2020-08-04 07:58:57 +0200

    Add privileges for mysql commands

--- INSTALL/INSTALL.sh
+++ INSTALL/INSTALL.sh
@@ -1283,15 +1283,15 @@ prepareDB () {
       pw=${MISP_PASSWORD}
     fi
     # Make sure that NOBODY can access the server without a password
-	mysql -e "UPDATE mysql.user SET Password = PASSWORD('${DBPASSWORD_ADMIN}') WHERE User = 'root'"
+	sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('${DBPASSWORD_ADMIN}') WHERE User = 'root'"
 	# Kill the anonymous users
-	mysql -e "DROP USER ''@'localhost'"
+	sudo mysql -e "DROP USER ''@'localhost'"
 	# Because our hostname varies we'll use some Bash magic here.
-	mysql -e "DROP USER ''@'$(hostname)'"
+	sudo mysql -e "DROP USER ''@'$(hostname)'"
 	# Kill off the demo database
-	mysql -e "DROP DATABASE test"
+	sudo mysql -e "DROP DATABASE test"
 	# Make our changes take effect
-	mysql -e "FLUSH PRIVILEGES"
+	sudo mysql -e "FLUSH PRIVILEGES"
     sudo apt-get purge -y expect ; sudo apt autoremove -qy
   fi 
 
