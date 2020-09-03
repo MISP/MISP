f8dfd76301cd1b4288a2421f53cbc2abcd8e5c62
mokaddem <sami.mokaddem@circl.lu>
2020-09-03 08:16:56 +0200

    chg: [misp.js] Correctly check if the variable exists before comparing

--- app/webroot/js/misp.js
+++ app/webroot/js/misp.js
@@ -3872,7 +3872,7 @@ function attributeHoverPlacement(element) {
 $('body').on('click', function (e) {
   $('[data-toggle=popover]').each(function () {
     // hide any open popovers when the anywhere else in the body is clicked
-    if (currentPopover !== undefined && currentPopover !== '') {
+    if (typeof currentPopover !== 'undefined' && currentPopover !== '') {
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
           $('#' + currentPopover).popover('destroy');
         }
