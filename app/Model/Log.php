f56cab6e498816308d7cfe2e702e36529d0013c6
mokaddem <sami.mokaddem@circl.lu>
2020-09-18 16:51:53 +0200

    chg: [log] Convert shadowAtribute's fs/ls into understood format

--- app/Model/Log.php
+++ app/Model/Log.php
@@ -947,6 +947,7 @@ class Log extends AppModel
                 if (!empty($logEntry['data']['value2'])) {
                     $logEntry['data']['value'] .= '|' . $logEntry['data']['value2'];
                 }
+                $logEntry['data'] = $this->Attribute->UTCToISODatetime(['ShadowAttribute' => $logEntry['data']], 'ShadowAttribute');
                 if (!empty($this->mockRecovery)) {
                     $this->mockLog[] = ['model' => 'ShadowAttribute', 'action' => 'add', 'data' => $logEntry['data']];
                 } else {
@@ -1052,6 +1053,9 @@ class Log extends AppModel
 
     private function __executeRecoveryMispObject($logEntry)
     {
+        if (empty($this->Attribute)) {
+            $this->Attribute = ClassRegistry::init('Attribute');
+        }
         if (empty($this->MispObject)) {
             $this->MispObject = ClassRegistry::init('MispObject');
         }
