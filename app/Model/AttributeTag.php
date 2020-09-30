065eba4c6b1b96ea2310968eabcf980afbf829b3
iglocska <andras.iklody@gmail.com>
2020-09-30 17:47:40 +0200

    Revert "fix: [tag] Show correct count of tag attributes and events"
    This reverts commit e644f4ea4c01e1f8018133d2a82aa3c321fff98d.

--- app/Model/AttributeTag.php
+++ app/Model/AttributeTag.php
@@ -1,9 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
 
-/**
- * @property Attribute $Attribute
- */
 class AttributeTag extends AppModel
 {
     public $actsAs = array('Containable');
@@ -180,30 +177,11 @@ class AttributeTag extends AppModel
         }
     }
 
-    /**
-     * Count number of not deleted attributes that contains given tag for given user. Tag must contains 'AttributeTag'.
-     *
-     * @param array $tag
-     * @param array $user
-     * @return int
-     */
-    public function countForTag(array $tag, array $user)
+    public function countForTag($tag_id, $user)
     {
-        $attributeIds = [];
-        foreach ($tag['AttributeTag'] as $attributeTag) {
-            $attributeIds[] = $attributeTag['attribute_id'];
-        }
-
-        if (empty($attributeIds)) {
-            return 0;
-        }
-
-        $conditions = $this->Attribute->buildConditions($user);
-        $conditions['Attribute.id'] = $attributeIds;
-        $conditions['Attribute.deleted'] = 0;
-        return $this->Attribute->find('count', array(
-            'recursive' => 0,
-            'conditions' => $conditions,
+        return $this->find('count', array(
+            'recursive' => -1,
+            'conditions' => array('AttributeTag.tag_id' => $tag_id)
         ));
     }
 
