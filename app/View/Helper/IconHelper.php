119de231b5ff250d9bbbf50236c3b4a82bd7d78d
Jakub Onderka <jakub.onderka@gmail.com>
2020-10-10 20:15:31 +0200

    new: [UI] Use flag icons from Twemoji

--- app/View/Helper/IconHelper.php
+++ app/View/Helper/IconHelper.php
@@ -1,30 +1,30 @@
 <?php
 App::uses('AppHelper', 'View/Helper');
 
-class IconHelper extends AppHelper {
+class IconHelper extends AppHelper
+{
     /**
      * @param string $countryCode ISO 3166-1 alpha-2 two-letter country code
+     * @param string $countryName Full country name for title
      * @return string
      */
-    public function countryFlag($countryCode)
+    public function countryFlag($countryCode, $countryName = null)
     {
         if (strlen($countryCode) !== 2) {
             return '';
         }
 
-        return '<span class="famfamfam-flag-' . strtolower(h($countryCode)) . '" ></span>';
-        /* Unicode version
-        $output = '';
+        $output = [];
         foreach (str_split(strtolower($countryCode)) as $letter) {
             $letterCode = ord($letter);
             if ($letterCode < 97 || $letterCode > 122) {
                 return ''; // invalid letter
             }
-
-            // UTF-8 representation
-            $output .= "\xF0\x9F\x87" . chr(0xa6 + ($letterCode - 97));
+            $output[] = "1f1" . dechex(0xe6 + ($letterCode - 97));
         }
 
-        return $output;*/
+        $baseurl = $this->_View->viewVars['baseurl'];
+        $title = __('Flag of %s',  strtoupper($countryName ? h($countryName) : $countryCode));
+        return '<img src="' . $baseurl . '/img/flags/' . implode('-', $output) . '.svg" title="' . $title .'" alt="' . $title . '" aria-label="' . $title . '"  style="height: 18px" />';
     }
 }
