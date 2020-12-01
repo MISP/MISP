b0f9add117b692f5d7de8f669a764c7d0eb2e1b3
Golbark <loic.fortemps@gmail.com>
2020-12-01 13:51:45 +0100

    new: [internal] HTTP header with username in the reponse

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -333,6 +333,7 @@ class AppController extends Controller
             if ($this->Auth->user('Role')['perm_site_admin'] || (Configure::read('MISP.live') && !$this->_isRest())) {
                 $this->{$this->modelClass}->runUpdates();
             }
+            header('X-Username: '.$this->Auth->user('email'));
             $user = $this->Auth->user();
             if (!isset($user['force_logout']) || $user['force_logout']) {
                 $this->loadModel('User');
