526361277ed40d3f28e0e9b14e6ee9303a4d081c
Jakub Onderka <jakub.onderka@gmail.com>
2021-01-17 10:38:20 +0100

    chg: [internal] Do not fetch full clusters for rest event index

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -682,8 +682,7 @@ class EventsController extends AppController
         } else {
             $this->paginate['contain'] = array_merge($this->paginate['contain'], array('User.email'));
         }
-        $this->set('urlparams', $urlparams);
-        $this->set('passedArgsArray', $passedArgsArray);
+
         $this->paginate = Set::merge($this->paginate, array('contain' => array(
             'ThreatLevel' => array(
                 'fields' => array(
@@ -710,7 +709,6 @@ class EventsController extends AppController
             }
             $minimal = !empty($passedArgs['searchminimal']) || !empty($passedArgs['minimal']);
             if ($minimal) {
-                unset($rules['contain']);
                 $rules['recursive'] = -1;
                 $rules['fields'] = array('id', 'timestamp', 'sighting_timestamp', 'published', 'uuid');
                 $rules['contain'] = array('Orgc.uuid');
@@ -794,7 +792,7 @@ class EventsController extends AppController
                         }
                     }
                 }
-                $events = $this->GalaxyCluster->attachClustersToEventIndex($this->Auth->user(), $events);
+                $events = $this->GalaxyCluster->attachClustersToEventIndex($this->Auth->user(), $events, false, false);
                 foreach ($events as $key => $event) {
                     if (empty($event['SharingGroup']['name'])) {
                         unset($event['SharingGroup']);
@@ -822,31 +820,38 @@ class EventsController extends AppController
                 }
             }
             return $this->RestResponse->viewData($events, $this->response->type(), false, false, false, array('X-Result-Count' => $absolute_total));
-        } else {
-            $events = $this->paginate();
-            foreach ($events as $k => $event) {
-                if (empty($event['SharingGroup']['name'])) {
-                    unset($events[$k]['SharingGroup']);
-                }
+        }
+
+        $events = $this->paginate();
+
+        if (count($events) === 1 && isset($this->passedArgs['searchall'])) {
+            $this->redirect(array('controller' => 'events', 'action' => 'view', $events[0]['Event']['id']));
+        }
+
+        foreach ($events as $k => $event) {
+            if (empty($event['SharingGroup']['name'])) {
+                unset($events[$k]['SharingGroup']);
             }
-            if (count($events) == 1 && isset($this->passedArgs['searchall'])) {
-                $this->redirect(array('controller' => 'events', 'action' => 'view', $events[0]['Event']['id']));
-            }
-            $events = $this->Event->attachTagsToEvents($events);
-            if (Configure::read('MISP.showCorrelationsOnIndex')) {
-                $events = $this->Event->attachCorrelationCountToEvents($this->Auth->user(), $events);
-            }
-            if (Configure::read('MISP.showSightingsCountOnIndex')) {
-                $events = $this->Event->attachSightingsCountToEvents($this->Auth->user(), $events);
-            }
-            if (Configure::read('MISP.showProposalsCountOnIndex')) {
-                $events = $this->Event->attachProposalsCountToEvents($this->Auth->user(), $events);
-            }
-            if (Configure::read('MISP.showDiscussionsCountOnIndex')) {
-                $events = $this->Event->attachDiscussionsCountToEvents($this->Auth->user(), $events);
-            }
-            $events = $this->GalaxyCluster->attachClustersToEventIndex($this->Auth->user(), $events, true, false);
-            $this->set('events', $events);
+        }
+        $events = $this->Event->attachTagsToEvents($events);
+        if (Configure::read('MISP.showCorrelationsOnIndex')) {
+            $events = $this->Event->attachCorrelationCountToEvents($this->Auth->user(), $events);
+        }
+        if (Configure::read('MISP.showSightingsCountOnIndex')) {
+            $events = $this->Event->attachSightingsCountToEvents($this->Auth->user(), $events);
+        }
+        if (Configure::read('MISP.showProposalsCountOnIndex')) {
+            $events = $this->Event->attachProposalsCountToEvents($this->Auth->user(), $events);
+        }
+        if (Configure::read('MISP.showDiscussionsCountOnIndex')) {
+            $events = $this->Event->attachDiscussionsCountToEvents($this->Auth->user(), $events);
+        }
+        $events = $this->GalaxyCluster->attachClustersToEventIndex($this->Auth->user(), $events, true, false);
+
+        if ($this->params['ext'] === 'csv') {
+            App::uses('CsvExport', 'Export');
+            $export = new CsvExport();
+            return $this->RestResponse->viewData($export->eventIndex($events), 'csv');
         }
 
         $user = $this->Auth->user();
@@ -869,16 +874,16 @@ class EventsController extends AppController
                 $this->Flash->info(__('No GnuPG key set in your profile. To receive attributes in emails, submit your public key in your profile.'));
             }
         }
+
+        $this->set('events', $events);
         $this->set('eventDescriptions', $this->Event->fieldDescriptions);
         $this->set('analysisLevels', $this->Event->analysisLevels);
         $this->set('distributionLevels', $this->Event->distributionLevels);
         $this->set('shortDist', $this->Event->shortDist);
         $this->set('distributionData', $this->genDistributionGraph(-1));
-        if ($this->params['ext'] === 'csv') {
-            App::uses('CsvExport', 'Export');
-            $export = new CsvExport();
-            return $this->RestResponse->viewData($export->eventIndex($events), 'csv');
-        }
+        $this->set('urlparams', $urlparams);
+        $this->set('passedArgsArray', $passedArgsArray);
+
         if ($this->request->is('ajax')) {
             $this->autoRender = false;
             $this->layout = false;
