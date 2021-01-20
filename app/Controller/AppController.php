0d9e95679c194c451fbe6fbfeac6848d0fb19782
Raphaël Vinot <raphael@vinot.info>
2021-01-20 12:58:56 +0100

    chg: Bump PyMISP version

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -26,7 +26,7 @@ class AppController extends Controller
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName', 'DataPathCollector');
 
     private $__queryVersion = '120';
-    public $pyMispVersion = '2.4.135';
+    public $pyMispVersion = '2.4.137';
     public $phpmin = '7.2';
     public $phprec = '7.4';
     public $pythonmin = '3.6';
