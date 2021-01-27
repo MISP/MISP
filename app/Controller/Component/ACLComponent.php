7bbd509e186ea548804668750090e95637f10fd9
mokaddem <sami.mokaddem@circl.lu>
2021-01-27 13:28:07 +0100

    fix: [acl] Added missing ACL entry

--- app/Controller/Component/ACLComponent.php
+++ app/Controller/Component/ACLComponent.php
@@ -543,6 +543,7 @@ class ACLComponent extends Component
                     'generateCorrelation' => array(),
                     'index' => array('*'),
                     'view' => array('*'),
+                    'viewPicture' => array('*'),
             ),
             'sharingGroups' => array(
                     'add' => array('perm_sharing_group'),
