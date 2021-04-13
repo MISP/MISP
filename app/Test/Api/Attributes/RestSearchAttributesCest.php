99293debce163fa2730155cb1c8a4727e4b5eb80
Luciano Righetti <luciano.righetti@gmail.com>
2021-04-13 17:10:25 +0200

    add openapi spec for attributes rest search, add api test

---
+++ app/Test/Api/Attributes/RestSearchAttributesCest.php
@@ -0,0 +1,58 @@
+<?php
+
+use \Helper\Fixture\Data\AttributeFixture;
+use \Helper\Fixture\Data\EventFixture;
+
+class RestSearchAttributesCest
+{
+
+    private const URL = '/attributes/restSearch?returnFormat=json';
+
+    public function testIndexReturnsForbiddenWithoutAuthKey(ApiTester $I)
+    {
+        $I->sendPost(self::URL);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+    }
+
+    public function testRestSearchReturnsExpectedAttribute(ApiTester $I)
+    {
+        $eventId = 1;
+        $orgId = 10;
+        $userId = 10;
+
+        $I->haveAuthorizationKey($orgId, $userId);
+
+        $fakeEvent = EventFixture::fake([
+            'id' => $eventId,
+            'org_id' => $orgId,
+            'user_id' => $userId,
+        ]);
+        $I->haveInDatabase('events', $fakeEvent->toDatabase());
+
+        $fakeAttributeFoo = AttributeFixture::fake([
+            'event_id' => $eventId,
+            'value1' => 'foo'
+        ]);
+        $fakeAttributeBar = AttributeFixture::fake(['event_id' => $eventId, 'value1' => 'bar']);
+        $I->haveInDatabase('attributes', $fakeAttributeFoo->toDatabase());
+        $I->haveInDatabase('attributes', $fakeAttributeBar->toDatabase());
+
+        // TODO: add a more complex search e.g. use tags, timestamps/dates
+        $I->sendPost(self::URL, [
+            'page' => 1,
+            'limit' => 1,
+            'value' => 'foo'
+        ]);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['response' => ['Attribute' => $fakeAttributeFoo->toResponse()]]);
+    }
+}
