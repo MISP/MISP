fb1b711604d9d692f457d283689ea72c2bab7063
Luciano Righetti <luciano.righetti@gmail.com>
2021-04-16 11:07:28 +0200

    add api test files.

---
+++ app/Test/Api/Events/IndexEventsCest.php
@@ -0,0 +1,38 @@
+<?php
+
+use \Helper\Fixture\Data\EventFixture;
+use \Helper\Fixture\Data\UserFixture;
+
+class IndexEventsCest
+{
+
+    private const URL = '/events';
+
+    public function testIndexReturnsForbiddenWithoutAuthKey(ApiTester $I)
+    {
+        $I->sendGet(self::URL);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+    }
+
+    public function testIndexReturnsExpectedEvent(ApiTester $I)
+    {
+        $orgId = 1;
+
+        $I->haveAuthorizationKey($orgId, 1, UserFixture::ROLE_ADMIN);
+        $fakeEvent = EventFixture::fake(['org_id' => $orgId, 'orgc_id' => $orgId]);
+        $I->haveInDatabase('events', $fakeEvent->toDatabase());
+
+        $I->sendGet(self::URL);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson([$fakeEvent->toResponse()]);
+    }
+}
