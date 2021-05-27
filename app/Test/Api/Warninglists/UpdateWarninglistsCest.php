a1acb60a48a18bf0d542e27d99795fbf332c8e57
Luciano Righetti <luciano.righetti@gmail.com>
2021-05-27 16:04:37 +0200

    remove delete endpoint (not rest), add openapi spec for /warninglists/update, add api test

---
+++ app/Test/Api/Warninglists/UpdateWarninglistsCest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use \Helper\Fixture\Data\WarninglistFixture;
+use \Helper\Fixture\Data\UserFixture;
+
+class UpdateWarninglistCest
+{
+
+    private const URL = '/warninglists/update';
+
+    public function testUpdateReturnsForbiddenWithoutAuthKey(ApiTester $I): void
+    {
+        $I->sendPost(self::URL);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+    }
+
+    public function testUpdate(ApiTester $I): void
+    {
+        $orgId = 1;
+        $userId = 1;
+        $warninglistId = 1;
+        $I->haveAuthorizationKey($orgId, $userId, UserFixture::ROLE_ADMIN);
+
+        $I->sendPost(self::URL, ['id' => $warninglistId]);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(
+            [
+                'saved' => true,
+                'success' => true,
+                'url' => "/warninglists/update",
+            ]
+        );
+        $I->seeResponseMatchesJsonType([
+            'message' => 'string:regex(/^Successfully updated \d+ warninglists.$/)'
+        ]);
+    }
+}
