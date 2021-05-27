0f9215de4180039dbd3eee352f30b8e62dd59d53
Luciano Righetti <luciano.righetti@gmail.com>
2021-05-27 12:10:43 +0200

    add openapi spec for /warninglists/view/[warninglist_id], add api test

---
+++ app/Test/Api/Warninglists/ViewWarninglistCest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use \Helper\Fixture\Data\WarninglistFixture;
+use \Helper\Fixture\Data\UserFixture;
+
+class ViewWarninglistCest
+{
+
+    private const URL = '/warninglists/view/%s';
+
+    public function testViewReturnsForbiddenWithoutAuthKey(ApiTester $I): void
+    {
+        $warninglistId = 1;
+        $I->sendGet(sprintf(self::URL, $warninglistId));
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+    }
+
+    public function testViewIndexReturnsExpectedWarninglist(ApiTester $I): void
+    {
+        $orgId = 1;
+        $userId = 1;
+        $I->haveAuthorizationKey($orgId, $userId, UserFixture::ROLE_ADMIN);
+
+        $warninglistId = 1;
+        $fakeWarninglist = WarninglistFixture::fake(['id' => $warninglistId]);
+        $I->haveInDatabase('warninglists', $fakeWarninglist->toDatabase());
+
+        $I->sendGet(sprintf(self::URL, $warninglistId));
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['Warninglist' => $fakeWarninglist->toResponse()]);
+    }
+}
