309d4731a98d77d0a4fc9dfb73de137906a07a3a
Luciano Righetti <luciano.righetti@gmail.com>
2021-06-01 14:09:59 +0200

    add openapi spec for /tags/view/[tag_id], add api test

---
+++ app/Test/Api/Tags/ViewTagCest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use \Helper\Fixture\Data\TagFixture;
+use \Helper\Fixture\Data\UserFixture;
+
+class ViewTagCest
+{
+
+    private const URL = '/tags/view/%s';
+
+    public function testViewReturnsForbiddenWithoutAuthKey(ApiTester $I): void
+    {
+        $I->sendGet(self::URL);
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+    }
+
+    public function testViewReturnsExpectedTag(ApiTester $I): void
+    {
+        $orgId = 1;
+        $userId = 1;
+        $I->haveAuthorizationKey($orgId, $userId, UserFixture::ROLE_ADMIN);
+
+        $tagId = 1;
+        $fakeTag = TagFixture::fake(['id' => $tagId]);
+        $I->haveInDatabase('tags', $fakeTag->toDatabase());
+
+        $I->sendGet(sprintf(self::URL, $tagId));
+
+        $I->validateRequest();
+        $I->validateResponse();
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson($fakeTag->toResponse());
+    }
+}
