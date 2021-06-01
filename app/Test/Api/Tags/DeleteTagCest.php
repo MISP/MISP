f3726e26dfa845204c9a96f2cc87fcf63fb5bbfc
Luciano Righetti <luciano.righetti@gmail.com>
2021-06-01 15:33:01 +0200

    add openapi spec for /tags/delete/[tag_id], add api test

--- app/Test/Api/Tags/DeleteTagCest.php
+++ app/Test/Api/Tags/DeleteTagCest.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 use \Helper\Fixture\Data\TagFixture;
 use \Helper\Fixture\Data\UserFixture;
 
-class ViewTagCest
+class DeleteTagCest
 {
 
-    private const URL = '/tags/view/%s';
+    private const URL = '/tags/delete/%s';
 
-    public function testViewReturnsForbiddenWithoutAuthKey(ApiTester $I): void
+    public function testDeleteReturnsForbiddenWithoutAuthKey(ApiTester $I): void
     {
         $tagId = 1;
-        $I->sendGet(sprintf(self::URL, $tagId));
+        $I->sendPost(sprintf(self::URL, $tagId));
 
         $I->validateRequest();
         $I->validateResponse();
@@ -22,7 +22,7 @@ class ViewTagCest
         $I->seeResponseIsJson();
     }
 
-    public function testViewReturnsExpectedTag(ApiTester $I): void
+    public function testDelete(ApiTester $I): void
     {
         $orgId = 1;
         $userId = 1;
@@ -32,12 +32,18 @@ class ViewTagCest
         $fakeTag = TagFixture::fake(['id' => $tagId]);
         $I->haveInDatabase('tags', $fakeTag->toDatabase());
 
-        $I->sendGet(sprintf(self::URL, $tagId));
+        $I->sendPost(sprintf(self::URL, $tagId));
 
         $I->validateRequest();
         $I->validateResponse();
 
         $I->seeResponseCodeIs(200);
-        $I->seeResponseContainsJson($fakeTag->toResponse());
+        $I->seeResponseContainsJson(
+            [
+                'name' => 'Tag deleted.',
+                'message' => 'Tag deleted.'
+            ]
+        );
+        $I->cantSeeInDatabase('tags', ['id' => $tagId]);
     }
 }
