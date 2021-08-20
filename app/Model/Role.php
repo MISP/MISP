80f2cde44ac025915e1945160c209f1b73ebec33
Luciano Righetti <luciano.righetti@gmail.com>
2021-08-20 11:26:10 +0200

    fix: fix query to make it work on all supported db engines

--- app/Model/Role.php
+++ app/Model/Role.php
@@ -49,7 +49,7 @@ class Role extends AppModel
     );
 
     public $virtualFields = array(
-        'permission' => "CASE WHEN (CAST(Role.perm_add AS INT) + CAST(Role.perm_modify AS INT) + CAST(Role.perm_publish AS INT) = 3) THEN '3' WHEN (CAST(Role.perm_add AS INT) + CAST(Role.perm_modify_org AS INT) = 2) THEN '2' WHEN (CAST(Role.perm_add AS INT) = 1) THEN '1' ELSE '0' END",
+        'permission' => "CASE WHEN (Role.perm_add AND Role.perm_modify AND Role.perm_publish) THEN '3' WHEN (Role.perm_add AND Role.perm_modify_org) THEN '2' WHEN (Role.perm_add) THEN '1' ELSE '0' END",
     );
 
     public $permissionConstants = array(
