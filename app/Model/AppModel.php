e1895cba813e7004d5b13dfcb7af55b87d048bc6
Jakub Onderka <jakub.onderka@gmail.com>
2021-10-17 16:17:17 +0200

    chg: [internal] Optimise sightings

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -3272,4 +3272,20 @@ class AppModel extends Model
         }
         return null;
     }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    protected function pubToZmq($name)
+    {
+        static $zmqEnabled;
+        if ($zmqEnabled === null) {
+            $zmqEnabled = (bool)Configure::read('Plugin.ZeroMQ_enable');
+        }
+        if ($zmqEnabled) {
+            return Configure::read("Plugin.ZeroMQ_{$name}_notifications_enable");
+        }
+        return false;
+    }
 }
