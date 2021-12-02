72548fd9a432607676d22b5b82653f2b2f99e57d
iglocska <andras.iklody@gmail.com>
2021-12-02 14:03:20 +0100

    fix: [UI] Ajax forms lose persistence
    - generic Form builder now has the persistence baked in
    - capture all form fields' data before submiting as expected

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '131';
+    private $__queryVersion = '132';
     public $pyMispVersion = '2.4.151';
     public $phpmin = '7.2';
     public $phprec = '7.4';
