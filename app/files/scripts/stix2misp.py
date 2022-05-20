96e77b38b1be37276beee0cd5c9a35f30404ea88
Christian Studer <christian.studer@circl.lu>
2022-05-20 17:12:34 +0200

    fix: [stix import] Avoiding non RFC-4122 UUIDs to be imported (and therefore skipped)

--- app/files/scripts/stix2misp.py
+++ app/files/scripts/stix2misp.py
@@ -42,6 +42,8 @@ except ImportError:
 
 _MISP_dir = "/".join([p for p in os.path.dirname(os.path.realpath(__file__)).split('/')[:-3]])
 _MISP_objects_path = '{_MISP_dir}/app/files/misp-objects/objects'.format(_MISP_dir=_MISP_dir)
+_RFC_UUID_VERSIONS = (1, 3, 4, 5)
+_UUIDv4 = uuid.UUID('76beed5f-7251-457e-8c2a-b45f7b589d3d')
 
 from pymisp.mispevent import MISPEvent, MISPObject, MISPAttribute
 
@@ -94,9 +96,28 @@ class StixParser():
     # Convert the MISP event we create from the STIX document into json format
     # and write it in the output file
     def saveFile(self):
+        for attribute in self.misp_event.attributes:
+            if uuid.UUID(attribute.uuid).version not in _RFC_UUID_VERSIONS:
+                attribute.uuid = self._sanitize_uuid(attribute)
+        for misp_object in self.misp_event.objects:
+            if uuid.UUID(misp_object.uuid).version not in _RFC_UUID_VERSIONS:
+                misp_object.uuid = self._sanitize_uuid(misp_object)
+                for reference in misp_object.references:
+                    reference.object_uuid = misp_object.uuid
+                    if uuid.UUID(reference.referenced_uuid).version not in _RFC_UUID_VERSIONS:
+                        reference.referenced_uuid = uuid.uuid5(_UUIDv4, reference.referenced_uuid)
+                for attribute in misp_object.attributes:
+                    if uuid.UUID(attribute.uuid).version not in _RFC_UUID_VERSIONS:
+                        attribute.uuid = self._sanitize_uuid(attribute)
         eventDict = self.misp_event.to_json()
         with open(self.outputname, 'wt', encoding='utf-8') as f:
             f.write(eventDict)
+
+    @staticmethod
+    def _sanitize_uuid(misp_feature):
+        comment = f'Original UUID was: {misp_feature.uuid}'
+        misp_feature.comment = f'{misp_feature.comment} - {comment}' if hasattr(misp_feature, 'comment') else comment
+        return uuid.uuid5(_UUIDv4, misp_feature.uuid)
 
     def parse_marking(self, handling):
         tags = []
