e70f6de2e94660f7901f430eb7876cf57b8367c3
iglocska <andras.iklody@gmail.com>
2022-08-01 10:09:22 +0200

    fix: [ACL] added overcorrelations

--- app/Controller/Component/ACLComponent.php
+++ app/Controller/Component/ACLComponent.php
@@ -98,6 +98,7 @@ class ACLComponent extends Component
             ],
             'correlations' => [
                 'generateTopCorrelations' => [],
+                'overCorrelations' => [],
                 'top' => []
             ],
             'cryptographicKeys' => [
