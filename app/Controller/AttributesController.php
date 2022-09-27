ef0581a1d141f569471edc11ae8dcace7e98402f
Jakub Onderka <jakub.onderka@gmail.com>
2022-09-27 15:59:36 +0200

    fix: [internal] Remove duplicate attribute fetching

--- app/Controller/AttributesController.php
+++ app/Controller/AttributesController.php
@@ -2815,10 +2815,7 @@ class AttributesController extends AppController
                 'fields' => ['Attribute.deleted', 'Attribute.event_id', 'Attribute.id', 'Attribute.object_id', 'Event.orgc_id', 'Event.user_id'],
                 'contain' => ['Event'],
             ]);
-            if (empty($attribute)) {
-                throw new NotFoundException(__('Invalid attribute'));
-            }
-            if ($attribute['Attribute']['deleted']) {
+            if (empty($attribute) || $attribute['Attribute']['deleted']) {
                 throw new NotFoundException(__('Invalid attribute'));
             }
             if (empty($tag_id)) {
@@ -2848,19 +2845,19 @@ class AttributesController extends AppController
             if (!$this->__canModifyTag($attribute, !empty($attributeTag['AttributeTag']['local']))) {
                 return new CakeResponse(array('body'=> json_encode(array('saved' => false, 'errors' => 'You do not have permission to do that.')), 'status' => 200, 'type' => 'json'));
             }
-
             if (empty($attributeTag)) {
                 return new CakeResponse(array('body'=> json_encode(array('saved' => false, 'errors' => 'Invalid attribute - tag combination.')), 'status' => 200, 'type' => 'json'));
             }
-            $tag = $this->Attribute->AttributeTag->Tag->find('first', array(
-                'conditions' => array('Tag.id' => $tag_id),
-                'recursive' => -1,
-                'fields' => array('Tag.name')
-            ));
             if ($this->Attribute->AttributeTag->delete($attributeTag['AttributeTag']['id'])) {
                 if (empty($attributeTag['AttributeTag']['local'])) {
                     $this->Attribute->touch($attribute);
                 }
+
+                $tag = $this->Attribute->AttributeTag->Tag->find('first', array(
+                    'conditions' => array('Tag.id' => $tag_id),
+                    'recursive' => -1,
+                    'fields' => array('Tag.name')
+                ));
                 $log = ClassRegistry::init('Log');
                 $log->createLogEntry($this->Auth->user(), 'tag', 'Attribute', $id, 'Removed tag (' . $tag_id . ') "' . $tag['Tag']['name'] . '" from attribute (' . $id . ')', 'Attribute (' . $id . ') untagged of Tag (' . $tag_id . ')');
                 return new CakeResponse(array('body'=> json_encode(array('saved' => true, 'success' => 'Tag removed.', 'check_publish' => empty($attributeTag['AttributeTag']['local']))), 'status' => 200, 'type'=> 'json'));
