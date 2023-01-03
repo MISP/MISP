faa1fc5300fc8e1e6e98c72112801625e583a3fb
Andras Iklody <andras.iklody@gmail.com>
2023-01-03 12:20:35 +0100

    fix: [querystring] bumped

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '146';
+    private $__queryVersion = '147';
     public $pyMispVersion = '2.4.167';
     public $phpmin = '7.2';
     public $phprec = '7.4';
