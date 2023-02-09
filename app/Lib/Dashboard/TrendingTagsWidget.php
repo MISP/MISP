44dac20bf0e301cc44019b93d7761df9f8602afc
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-02-09 10:06:11 +0100

    chg: [dashboard-widget:TrendingTags] Added filtering and over time functionalities

--- app/Lib/Dashboard/TrendingTagsWidget.php
+++ app/Lib/Dashboard/TrendingTagsWidget.php
@@ -10,14 +10,17 @@ class TrendingTagsWidget
         'time_window' => 'The time window, going back in seconds, that should be included.',
         'exclude' => 'List of substrings to exclude tags by - for example "sofacy" would exclude any tag containing sofacy.',
         'include' => 'List of substrings to include tags by - for example "sofacy" would include any tag containing sofacy.',
-        'threshold' => 'Limits the number of displayed tags. Default: 10'
+        'threshold' => 'Limits the number of displayed tags. Default: 10',
+        'filter_event_tags' => 'Filters to be applied on event tags',
+        'over_time' => 'Toggle the trending to be over time',
     );
     public $placeholder =
-'{
+    '{
     "time_window": "86400",
     "threshold": 15,
     "exclude": ["tlp:", "pap:"],
-    "include": ["misp-galaxy:", "my-internal-taxonomy"]
+    "include": ["misp-galaxy:", "my-internal-taxonomy"],
+    "filter_event_tags": ["misp-galaxy:threat-actor="APT 29"],
 }';
     public $description = 'Widget showing the trending tags over the past x seconds, along with the possibility to include/exclude tags.';
     public $cacheLifetime = 600;
@@ -30,33 +33,85 @@ class TrendingTagsWidget
         $params = [
             'timestamp' => time() - (empty($options['time_window']) ? 8640000 : $options['time_window']),
         ];
+        if (!empty($options['filter_event_tags'])) {
+            $params['event_tags'] = $options['filter_event_tags'];
+        }
         $eventIds = $eventModel->filterEventIds($user, $params);
 
-        $tags = [];
         $tagColours = [];
-        if (!empty($eventIds)) {
-            $eventTags = $eventModel->EventTag->find('all', [
-                'conditions' => ['EventTag.event_id' => $eventIds],
-                'contain' => ['Tag' => ['fields' => ['name', 'colour']]],
-                'recursive' => -1,
-                'fields' => ['id'],
-            ]);
+        $allTags = [];
+        if (!empty($options['over_time'])) {
+            $this->render = 'MultiLineChart';
 
-            foreach ($eventTags as $eventTag) {
-                $tagName = $eventTag['Tag']['name'];
-                if (isset($tags[$tagName])) {
-                    $tags[$tagName]++;
-                } else if ($this->checkTag($options, $tagName)) {
-                    $tags[$tagName] = 1;
-                    $tagColours[$tagName] = $eventTag['Tag']['colour'];
+            $tagOvertime = [];
+            if (!empty($eventIds)) {
+                $events = $eventModel->fetchEvent($user, [
+                    'eventid' => $eventIds,
+                    'order' => 'Event.timestamp',
+                ]);
+
+                foreach ($events as $event) {
+                    $timestamp = $event['Event']['timestamp'];
+                    $timestamp = strftime('%Y-%m-%d', $timestamp);
+                    foreach ($event['EventTag'] as $tag) {
+                        $tagName = $tag['Tag']['name'];
+                        if (isset($tagOvertime[$timestamp][$tagName])) {
+                            $tagOvertime[$timestamp][$tagName]++;
+                        } else if ($this->checkTag($options, $tagName)) {
+                            $tagOvertime[$timestamp][$tagName] = 1;
+                            $tagColours[$tagName] = $tag['Tag']['colour'];
+                            $allTags[$tagName] = $tagName;
+                        }
+                    }
                 }
             }
 
-            arsort($tags);
+            $data = [];
+            $data['data'] = [];
+            foreach($tagOvertime as $date => $tagCount) {
+                $item = [];
+                $item['date'] = $date;
+                foreach ($allTags as $tagName) {
+                    if (!empty($tagCount[$tagName])) {
+                        $item[$tagName] = $tagCount[$tagName];
+                    } else {
+                        $item[$tagName] = 0;
+                    }
+                }
+                $data['data'][] = $item;
+            }
+            uasort($data['data'], function ($a, $b) {
+                return ($a['date'] < $b['date']) ? -1 : 1;
+            });
+            $data['data'] = array_values($data['data']);
+            return $data;
+        } else {
+            $tags = [];
+            if (!empty($eventIds)) {
+                $eventTags = $eventModel->EventTag->find('all', [
+                    'conditions' => ['EventTag.event_id' => $eventIds],
+                    'contain' => ['Tag' => ['fields' => ['name', 'colour']]],
+                    'recursive' => -1,
+                    'fields' => ['id'],
+                ]);
+    
+                foreach ($eventTags as $eventTag) {
+                    $tagName = $eventTag['Tag']['name'];
+                    if (isset($tags[$tagName])) {
+                        $tags[$tagName]++;
+                    } else if ($this->checkTag($options, $tagName)) {
+                        $tags[$tagName] = 1;
+                        $tagColours[$tagName] = $eventTag['Tag']['colour'];
+                    }
+                }
+    
+                arsort($tags);
+                $data['data'] = array_slice($tags, 0, $threshold);
+                $data['colours'] = $tagColours;
+            }
+    
         }
 
-        $data['data'] = array_slice($tags, 0, $threshold);
-        $data['colours'] = $tagColours;
         return $data;
 	}
 
