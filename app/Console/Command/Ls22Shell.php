5720f258881626795b0e3608cda462a29c311664
iglocska <andras.iklody@gmail.com>
2023-03-08 10:08:00 +0100

    fix: [LS22 shell] parameter name fixed

--- app/Console/Command/Ls22Shell.php
+++ app/Console/Command/Ls22Shell.php
@@ -120,7 +120,7 @@ class Ls22Shell extends AppShell
                         'short' => 's',
                         'required' => true
                     ],
-                    'json' => [
+                    'value' => [
                         'help' => 'The value to set for the given setting',
                         'short' => 'v',
                         'required' => true
