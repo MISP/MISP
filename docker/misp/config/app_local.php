3e3daa9aa86a24e617d8a3d1376cd14898b2a8b4
Luciano Righetti <luciano.righetti@gmail.com>
2023-04-05 17:14:40 +0200

    fix: salt is required

--- docker/misp/config/app_local.php
+++ docker/misp/config/app_local.php
@@ -28,6 +28,17 @@ return [
     'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
 
     /*
+     * Security and encryption configuration
+     *
+     * - salt - A random string used in security hashing methods.
+     *   The salt value is also used as the encryption key.
+     *   You should treat it as extremely sensitive data.
+     */
+    'Security' => [
+        'salt' => env('SECURITY_SALT', '__SALT__'),
+    ],
+
+    /*
      * Connection information used by the ORM to connect
      * to your application's datastores.
      *
