362156daca3b6609fc0e226f663a242eeb26e1a8
Sami Mokaddem <sami.mokaddem@circl.lu>
2023-04-06 15:24:15 +0200

    fix: [event:viewAttribute] Reset pagination state when using a filter on the attribute table
    - This will certainly make @rommelfs happy :)

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '147';
+    private $__queryVersion = '148';
     public $pyMispVersion = '2.4.169.3';
     public $phpmin = '7.2';
     public $phprec = '7.4';
