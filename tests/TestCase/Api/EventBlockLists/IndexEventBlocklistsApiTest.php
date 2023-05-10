59ab13921abfedb352435acf40af6b82607f2ef2
Luciano Righetti <luciano.righetti@gmail.com>
2023-05-10 15:58:01 +0200

    fix: namespace

--- tests/TestCase/Api/EventBlockLists/IndexEventBlocklistsApiTest.php
+++ tests/TestCase/Api/EventBlockLists/IndexEventBlocklistsApiTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Test\TestCase\Api\Users;
+namespace App\Test\TestCase\Api\EventBlocklists;
 
 use Cake\TestSuite\TestCase;
 use App\Test\Fixture\AuthKeysFixture;
