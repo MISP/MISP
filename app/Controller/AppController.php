6a1963930ba9f2f6bdc332c12d1cc98752e8c4e3
Raphaël Vinot <raphael@vinot.info>
2023-06-08 15:14:58 +0200

    chg: [PyMISP] Bump

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -34,7 +34,7 @@ class AppController extends Controller
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
     private $__queryVersion = '150';
-    public $pyMispVersion = '2.4.171';
+    public $pyMispVersion = '2.4.172';
     public $phpmin = '7.2';
     public $phprec = '7.4';
     public $phptoonew = '8.0';
