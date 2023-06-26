404c71ade67033911b51c6c8198e0e5def8de80d
iglocska <andras.iklody@gmail.com>
2023-06-26 18:16:31 +0200

    new: [dashboard widget] added functionalities to download widget raw data
    - download the JSON passed to the front-end of a widget on-demand

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '151';
+    private $__queryVersion = '152';
     public $pyMispVersion = '2.4.172';
     public $phpmin = '7.2';
     public $phprec = '7.4';
