ea32fef68c8599a77da453ba285adfa46e0cbf2f
vincenzocaputo <32276363+vincenzocaputo@users.noreply.github.com>
2023-07-23 17:48:59 +0200

    Add sighting publish workflow trigger

--- app/Model/Sighting.php
+++ app/Model/Sighting.php
@@ -79,7 +79,8 @@ class Sighting extends AppModel
     {
         $pubToZmq = $this->pubToZmq('sighting');
         $kafkaTopic = $this->kafkaTopic('sighting');
-        if ($pubToZmq || $kafkaTopic) {
+        $isTriggerCallable = $this->isTriggerCallable('sighting-publish');
+        if ($pubToZmq || $kafkaTopic || $isTriggerCallable) {
             $sighting = $this->getSighting($this->id);
             if ($pubToZmq) {
                 $pubSubTool = $this->getPubSubTool();
@@ -88,6 +89,17 @@ class Sighting extends AppModel
             if ($kafkaTopic) {
                 $kafkaPubTool = $this->getKafkaPubTool();
                 $kafkaPubTool->publishJson($kafkaTopic, $sighting, 'add');
+            }
+
+            if($isTriggerCallable) {
+                $workflowErrors = [];
+                $logging = [
+                    'model' => 'Sighting',
+                    'action' => $action,
+                    'id' => $sighting['Sighting']['id'],
+                ];
+                $triggerData = $sighting;
+                $this->executeTrigger('sighting-publish', $triggerData, $workflowErrors, $logging);
             }
         }
         return true;
