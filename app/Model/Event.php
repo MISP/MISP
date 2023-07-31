MISP/MISP

commit 64580168622aeea59997cea5739cf0b8dbcf8bda

chg: [event:publishSightingsRouter] Change from prio worker to default
There is no need to keep this task in prio as sightings are not sync inline anymore.

app/Model/Event.php
@@ -4501,7 +4501,7 @@ class Event extends AppModel
             /** @var Job $job */
             $job = ClassRegistry::init('Job');
             $message = empty($sightingUuids) ? __('Publishing sightings.') : __('Publishing %s sightings.', count($sightingUuids));
-            $jobId = $job->createJob($user, Job::WORKER_PRIO, 'publish_event', "Event ID: $id", $message);
+            $jobId = $job->createJob($user, Job::WORKER_DEFAULT, 'publish_event', "Event ID: $id", $message);
 
             $args = ['publish_sightings', $id, $passAlong, $jobId, $user['id']];
             if (!empty($sightingUuids)) {

commit 2f4a49ccb2433c7bc458eea6a03645295d27a9b8

fix: [event:publishSightingsRouter] Make sure to use correct queue for publishSightingsRouter
Fix bug introduced in 64580168622aeea59997cea5739cf0b8dbcf8bda where workers were set to default but not the queue

app/Model/Event.php
@@ -4511,7 +4511,7 @@ class Event extends AppModel
             }
 
             return $this->getBackgroundJobsTool()->enqueue(
-                BackgroundJobsTool::PRIO_QUEUE,
+                BackgroundJobsTool::DEFAULT_QUEUE,
                 BackgroundJobsTool::CMD_EVENT,
                 $args,
                 true,