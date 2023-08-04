a8239d2c89b0af5827b606b1c77ac67155736393
Sami Mokaddem <sami.mokaddem@circl.lu>
2023-08-04 14:29:41 +0200

    chg: [sharingGroup] Added navigation link to sharing-group-template

--- src/Controller/Component/Navigation/SharingGroups.php
+++ src/Controller/Component/Navigation/SharingGroups.php
@@ -5,4 +5,12 @@ require_once(APP . 'Controller' . DS . 'Component' . DS . 'Navigation' . DS . 'b
 
 class SharingGroupsNavigation extends BaseNavigation
 {
+
+    public function addLinks()
+    {
+        $this->bcf->addCustomLink('SharingGroups', 'index', '/sharing-group-blueprints', __('List Sharing Group Blueprints'), [
+            'icon' => 'ruler',
+        ]);
+    }
+
 }
