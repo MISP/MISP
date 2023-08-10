83a86e5141278be4d5fae49175c74223b7ea9771
Luciano Righetti <luciano.righetti@gmail.com>
2023-08-10 10:08:29 +0200

    fix: do not use hyphen in col name

---
+++ config/Migrations/20230809090207_rename_object_meta_category_column.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class RenameObjectMetaCategoryColumn extends AbstractMigration
+{
+    public function up()
+    {
+        $objectTemplates = $this->table('object_templates');
+        $objectTemplates->renameColumn('meta-category', 'meta_category');
+        $objectTemplates->save();
+        $objects = $this->table('objects');
+        $objects->renameColumn('meta-category', 'meta_category');
+        $objects->save();
+    }
+
+    public function down()
+    {
+        $objectTemplates = $this->table('object_templates');
+        $objectTemplates->renameColumn('meta_category', 'meta-category');
+        $objectTemplates->save();
+        $objects = $this->table('objects');
+        $objects->renameColumn('meta_category', 'meta-category');
+        $objects->save();
+    }
+}
