2b030fb4961a548b5d54a17dc4a6975141358480
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-10-17 22:34:57 +0200

    fix: [workflow-module:publish-event] Extend correct class and use event id

--- app/Model/WorkflowModules/action/Module_publish_event.php
+++ app/Model/WorkflowModules/action/Module_publish_event.php
@@ -1,13 +1,16 @@
 <?php
-include_once APP . 'Model/WorkflowModules/action/Module_webhook.php';
+include_once APP . 'Model/WorkflowModules/WorkflowBaseModule.php';
 
-class Module_publish_event extends Module_webhook
+class Module_publish_event extends WorkflowBaseActionModule
 {
     public $id = 'publish-event';
     public $name = 'Publish Event';
     public $version = '0.1';
     public $description = 'Publish an Event in the context of the workflow';
-    public $icon_path = 'upload';
+    public $icon = 'upload';
+    public $inputs = 1;
+    public $outputs = 1;
+    public $params = [];
 
     private $Event;
 
@@ -22,6 +25,8 @@ class Module_publish_event extends Module_webhook
 
     public function exec(array $node, WorkflowRoamingData $roamingData, array &$errors = []): bool
     {
+        $rData = $roamingData->getData();
+        $event_id = $rData['Event']['id'];
         $result = $this->Event->publish($event_id, null);
         return $result === true;
     }
