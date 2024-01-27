dcfb2fe8c4668abfe0b74996ab8dd22503179bb2
Jakub Onderka <jakub.onderka@gmail.com>
2024-01-27 09:56:30 +0100

    fix: [internal] Fetching latest remote Git version

--- app/Lib/Tools/GitTool.php
+++ app/Lib/Tools/GitTool.php
@@ -12,11 +12,7 @@ class GitTool
     public static function getLatestTags(HttpSocketExtended $HttpSocket)
     {
         $url = 'https://api.github.com/repos/MISP/MISP/tags?per_page=10';
-        $response = $HttpSocket->get($url);
-        if (!$response->isOk()) {
-            throw new HttpSocketHttpException($response, $url);
-        }
-        return $response->json();
+        return self::gitHubRequest($HttpSocket, $url);
     }
 
     /**
@@ -28,15 +24,27 @@ class GitTool
     public static function getLatestCommit(HttpSocketExtended $HttpSocket)
     {
         $url = 'https://api.github.com/repos/MISP/MISP/commits?per_page=1';
-        $response = $HttpSocket->get($url);
-        if (!$response->isOk()) {
-            throw new HttpSocketHttpException($response, $url);
-        }
-        $data = $response->json();
+        $data = self::gitHubRequest($HttpSocket, $url);
         if (!isset($data[0]['sha'])) {
             throw new Exception("Response do not contains requested data.");
         }
         return $data[0]['sha'];
+    }
+
+    /**
+     * @param HttpSocketExtended $HttpSocket
+     * @param string $url
+     * @return array
+     * @throws HttpSocketHttpException
+     * @throws HttpSocketJsonException
+     */
+    private static function gitHubRequest(HttpSocketExtended $HttpSocket, $url)
+    {
+        $response = $HttpSocket->get($url, [], ['header' => ['User-Agent' => 'MISP']]);
+        if (!$response->isOk()) {
+            throw new HttpSocketHttpException($response, $url);
+        }
+        return $response->json();
     }
 
     /**
