509708a2a27b7c48cd85f50ee341185dc2806ad0
Sami Mokaddem <mokaddem.sami@gmail.com>
2024-02-07 15:21:58 +0100

    chg: [app:queryVersion] Bumped version

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
 
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
-    private $__queryVersion = '158';
+    private $__queryVersion = '159';
     public $pyMispVersion = '2.4.184';
     public $phpmin = '7.2';
     public $phprec = '7.4';
