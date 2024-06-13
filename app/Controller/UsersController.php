069f658b8b47b25941dc5e772f9d60236b66da3e
iglocska <andras.iklody@gmail.com>
2024-06-13 13:54:58 +0200

    new: [users api] added new boolean field to the output indicating whether totp is set for the user
    - A simple boolean field to show whether totp has been set up for the given account
    - works for /users/view, /admin/users/view, /admin/users/index

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -92,6 +92,7 @@ class UsersController extends AppController
             unset($user['User']['authkey']);
         }
         $user['User']['password'] = '*****';
+        $user['User']['totp_is_set'] = !empty($user['User']['totp']);
         $user['User']['totp'] = '*****';
         $temp = [];
         $objectsToInclude = array('User', 'Role', 'UserSetting', 'Organisation');
@@ -476,7 +477,8 @@ class UsersController extends AppController
                     'force_logout',
                     'date_created',
                     'date_modified',
-                    'last_pw_change'
+                    'last_pw_change',
+                    'totp'
                 ),
                 'contain' => array(
                     'Organisation' => array('id', 'name'),
@@ -489,6 +491,10 @@ class UsersController extends AppController
                         $users[$key]['User']['authkey'] = __('Redacted');
                     }
                 }
+            }
+            foreach ($users as $key => $user) {
+                $users[$key]['User']['totp_is_set'] = !empty($user['User']['totp']);
+                unset($users[$key]['User']['totp']);
             }
             $users = $this->User->attachIsUserMonitored($users);
             return $this->RestResponse->viewData($users, $this->response->type());
