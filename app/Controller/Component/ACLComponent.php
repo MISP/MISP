ee281d5cd1bc7f88a6236ce444cc91ee498335d6
Andras Iklody <andras.iklody@gmail.com>
2024-07-10 14:28:24 +0200

    fix: [ACL] user add always accessible to site admins

--- app/Controller/Component/ACLComponent.php
+++ app/Controller/Component/ACLComponent.php
@@ -947,7 +947,7 @@ class ACLComponent extends Component
             return true;
         };
         $this->dynamicChecks['add_user_enabled'] = function (array $user) {
-            if (Configure::read('MISP.disable_user_add')) {
+            if (Configure::read('MISP.disable_user_add') && !$user['Role']['perm_site_admin']) {
                 throw new ForbiddenException('Adding users has been disabled on this instance.');
             }
             return true;
