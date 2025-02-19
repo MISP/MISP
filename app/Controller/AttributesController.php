4f290c876669a98bc234c53daa5133510317d005
Remigiusz Kollataj <remigiusz.kollataj@dreamlab.net>
2025-02-19 09:32:48 +0100

    Attribute tag removal: add missing field when fetching attribute to fix MispAttribute->touch()
    Signed-off-by: Remigiusz Kollataj <remigiusz.kollataj@dreamlab.net>

--- app/Controller/AttributesController.php
+++ app/Controller/AttributesController.php
@@ -2809,7 +2809,7 @@ class AttributesController extends AppController
             $attribute = $this->MispAttribute->find('first', [
                 'recursive' => -1,
                 'conditions' => ['Attribute.id' => $id],
-                'fields' => ['Attribute.deleted', 'Attribute.event_id', 'Attribute.id', 'Attribute.object_id', 'Event.orgc_id', 'Event.user_id'],
+                'fields' => ['Attribute.deleted', 'Attribute.event_id', 'Attribute.id', 'Attribute.object_id', 'Attribute.type', 'Event.orgc_id', 'Event.user_id'],
                 'contain' => ['Event'],
             ]);
             if (empty($attribute) || $attribute['Attribute']['deleted']) {
