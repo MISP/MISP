4ad9de035568727e12a71067f601e785f9e40273
Jakub Onderka <jakub.onderka@gmail.com>
2025-03-28 09:57:05 +0100

    chg: [internal] Optimise handling compressed requests, add support for zstd

--- app/Controller/Component/CompressedRequestHandlerComponent.php
+++ app/Controller/Component/CompressedRequestHandlerComponent.php
@@ -1,6 +1,9 @@
 <?php
 class CompressedRequestHandlerComponent extends Component
 {
+    /**
+     * @throws Exception
+     */
     public function initialize(Controller $controller)
     {
         if ($controller instanceof CakeErrorController) {
@@ -9,10 +12,12 @@ class CompressedRequestHandlerComponent extends Component
 
         $contentEncoding = $_SERVER['HTTP_CONTENT_ENCODING'] ?? null;
         if (!empty($contentEncoding)) {
-            if ($contentEncoding === 'br') {
-                $controller->request->setInput($this->decodeBrotliEncodedContent($controller));
+            if ($contentEncoding === 'zstd') {
+                $controller->request->setInput($this->decodeZstdEncodedContent());
+            } else if ($contentEncoding === 'br') {
+                $controller->request->setInput($this->decodeBrotliEncodedContent());
             } else if ($contentEncoding === 'gzip') {
-                $controller->request->setInput($this->decodeGzipEncodedContent($controller));
+                $controller->request->setInput($this->decodeGzipEncodedContent());
             } else {
                 throw new BadRequestException("Unsupported content encoding '$contentEncoding'.");
             }
@@ -31,6 +36,9 @@ class CompressedRequestHandlerComponent extends Component
         if (function_exists('brotli_uncompress')) {
             $supportedEncodings[] = 'br';
         }
+        if (function_exists('zstd_uncompress')) {
+            $supportedEncodings[] = 'zstd';
+        }
         return $supportedEncodings;
     }
 
@@ -38,14 +46,10 @@ class CompressedRequestHandlerComponent extends Component
      * @return string
      * @throws Exception
      */
-    private function decodeGzipEncodedContent(Controller $controller)
+    private function decodeGzipEncodedContent()
     {
         if (function_exists('gzdecode')) {
-            $input = $controller->request->input();
-            if (empty($input)) {
-                throw new BadRequestException('Request data should be gzip encoded, but request is empty.');
-            }
-            $decoded = gzdecode($input);
+            $decoded = gzdecode($this->input());
             if ($decoded === false) {
                 throw new BadRequestException('Invalid compressed data.');
             }
@@ -55,14 +59,13 @@ class CompressedRequestHandlerComponent extends Component
     }
 
     /**
-     * @param Controller $controller
      * @return string
      * @throws Exception
      */
-    private function decodeBrotliEncodedContent(Controller $controller)
+    private function decodeBrotliEncodedContent()
     {
         if (function_exists('brotli_uncompress')) {
-            $decoded = brotli_uncompress($controller->request->input());
+            $decoded = brotli_uncompress($this->input());
             if ($decoded === false) {
                 throw new BadRequestException('Invalid compressed data.');
             }
@@ -70,5 +73,38 @@ class CompressedRequestHandlerComponent extends Component
         } else {
             throw new BadRequestException("This server doesn't support brotli compressed requests.");
         }
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    private function decodeZstdEncodedContent()
+    {
+        if (function_exists('zstd_uncompress')) {
+            $decoded = zstd_uncompress($this->input());
+            if ($decoded === false) {
+                throw new BadRequestException('Invalid compressed data.');
+            }
+            return $decoded;
+        } else {
+            throw new BadRequestException("This server doesn't support zstd compressed requests.");
+        }
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    private function input()
+    {
+        $input = file_get_contents('php://input');
+        if ($input === false) {
+            throw new Exception("Could not read input");
+        }
+        if (empty($input)) {
+            throw new BadRequestException('Request data should be encoded, but request is empty.');
+        }
+        return $input;
     }
 }
