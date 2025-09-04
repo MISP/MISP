3f8281c98c0bb88794496d9d6a01e2fb5f5a1686
iglocska <andras.iklody@gmail.com>
2025-09-04 11:55:03 +0200

    fix: [acl] name fixed for the recorrelation action to please the CI gods

--- app/Controller/Component/ACLComponent.php
+++ app/Controller/Component/ACLComponent.php
@@ -333,7 +333,7 @@ class ACLComponent extends Component
             'pushEventToKafka' => array('perm_publish_kafka'),
             'pushProposals' => array('perm_sync'),
             'queryEnrichment' => array('perm_add'),
-            'recorrelate' => [],
+            'recorrelateEvent' => [],
             'recoverEvent' => array(),
             'removePivot' => array('*'),
             'removeTag' => array('perm_tagger'),
