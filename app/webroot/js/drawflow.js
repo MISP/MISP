94079773679a29b3f722ea105cf8f100078a0de6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-09 02:56:39 -0400

    spelling: completename
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/webroot/js/drawflow.js
+++ app/webroot/js/drawflow.js
@@ -1377,7 +1377,7 @@ class Drawflow {
       }
     })
 
-    function insertObjectkeys(object, name, completname) {
+    function insertObjectkeys(object, name, completename) {
       if (object === null) {
         var object = data[name];
       } else {
@@ -1386,9 +1386,9 @@ class Drawflow {
       if (object !== null) {
         Object.entries(object).forEach(function (key, value) {
           if (typeof key[1] === "object") {
-            insertObjectkeys(object, key[0], completname + '-' + key[0]);
+            insertObjectkeys(object, key[0], completename + '-' + key[0]);
           } else {
-            var elems = content.querySelectorAll('[df-' + completname + '-' + key[0] + ']');
+            var elems = content.querySelectorAll('[df-' + completename + '-' + key[0] + ']');
             for (var i = 0; i < elems.length; i++) {
               elems[i].value = key[1];
               if (elems[i].isContentEditable) {
@@ -1520,7 +1520,7 @@ class Drawflow {
       }
     })
 
-    function insertObjectkeys(object, name, completname) {
+    function insertObjectkeys(object, name, completename) {
       if (object === null) {
         var object = dataNode.data[name];
       } else {
@@ -1529,9 +1529,9 @@ class Drawflow {
       if (object !== null) {
         Object.entries(object).forEach(function (key, value) {
           if (typeof key[1] === "object") {
-            insertObjectkeys(object, key[0], completname + '-' + key[0]);
+            insertObjectkeys(object, key[0], completename + '-' + key[0]);
           } else {
-            var elems = content.querySelectorAll('[df-' + completname + '-' + key[0] + ']');
+            var elems = content.querySelectorAll('[df-' + completename + '-' + key[0] + ']');
             for (var i = 0; i < elems.length; i++) {
               elems[i].value = key[1];
               if (elems[i].isContentEditable) {
@@ -1744,7 +1744,7 @@ class Drawflow {
         }
       })
 
-      function insertObjectkeys(object, name, completname) {
+      function insertObjectkeys(object, name, completename) {
         if (object === null) {
           var object = data[name];
         } else {
@@ -1753,9 +1753,9 @@ class Drawflow {
         if (object !== null) {
           Object.entries(object).forEach(function (key, value) {
             if (typeof key[1] === "object") {
-              insertObjectkeys(object, key[0], completname + '-' + key[0]);
+              insertObjectkeys(object, key[0], completename + '-' + key[0]);
             } else {
-              var elems = content.querySelectorAll('[df-' + completname + '-' + key[0] + ']');
+              var elems = content.querySelectorAll('[df-' + completename + '-' + key[0] + ']');
               for (var i = 0; i < elems.length; i++) {
                 elems[i].value = key[1];
                 if (elems[i].isContentEditable) {
