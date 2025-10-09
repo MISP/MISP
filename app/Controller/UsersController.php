898e899b6eb1a98419034f68ad6dcf7f2e54463c
iglocska <andras.iklody@gmail.com>
2025-10-09 07:52:56 +0200

    fix: [user edit] don't load related models when retrieving the user for editing via the GUI
    - it lead to fetching all related event meta information, which can be a memory hog

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -871,7 +871,7 @@ class UsersController extends AppController
         $userToEdit = $this->User->find('first', array(
             'conditions' => $this->__adminFetchConditions($id),
             'recursive' => -1,
-            'fields' => array('User.id', 'User.role_id', 'User.email', 'User.org_id', 'Role.perm_site_admin'),
+            'fields' => array('User.*', 'Role.perm_site_admin'),
             'contain' => array('Role')
         ));
         if (empty($userToEdit)) {
@@ -1077,12 +1077,11 @@ class UsersController extends AppController
             if ($this->_isRest()) {
                 return $this->RestResponse->describe('Users', 'admin_edit', $id, $this->response->type());
             }
-            $this->User->read(null, $id);
             if (!$this->_isSiteAdmin() && $this->Auth->user('org_id') != $this->User->data['User']['org_id']) {
                 $this->redirect(array('controller' => 'users', 'action' => 'index', 'admin' => true));
             }
-            $this->User->set('password', '');
-            $this->request->data = $this->User->data;
+            $this->request->data = $userToEdit;
+            $this->request->data['User']['password'] = '';
         }
         if ($this->_isSiteAdmin()) {
             $orgs = $this->User->Organisation->find('list', array(
