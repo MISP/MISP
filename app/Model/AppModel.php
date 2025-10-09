a8e5c5d3633951a03e92b86feda2e14d13c38f21
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-09 14:36:21 -0400

    spelling: function
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -109,7 +109,7 @@ class AppModel extends Model
             'requirements' => 'MySQL version must be >= 5.6', # message stating the requirements necessary for the update
             'record' => false, # should the update success be saved in the admin_table
             // 'preUpdate' => 'seenOnAttributeAndObjectPreUpdate', # Function to execute before the update. If it throws an error, it cancels the update
-            'url' => '/servers/updateDatabase/seenOnAttributeAndObject/' # url pointing to the funcion performing the update
+            'url' => '/servers/updateDatabase/seenOnAttributeAndObject/' # url pointing to the function performing the update
         ),
     );
 
