cd7ea44beb90a633058e8cab79eaed0bf4039df7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-09 03:20:16 -0400

    spelling: genuine
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/Controller/Component/RestResponseComponent.php
+++ app/Controller/Component/RestResponseComponent.php
@@ -1372,7 +1372,7 @@ class RestResponseComponent extends Component
                 'type' => 'string',
                 'operators' => array('equal'),
                 'values' => array( 'network' => 'Network', 'local' => 'Local'),
-                'help' => __('Specify whether the source (url field) is a directory (local) or a geniun url (network)')
+                'help' => __('Specify whether the source (url field) is a directory (local) or a genuine url (network)')
             ),
             'ip' => array(
                 'input' => 'text',
