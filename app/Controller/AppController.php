b453e58249a130b542b5f4977128994cf7913d94
iglocska <andras.iklody@gmail.com>
2025-12-09 10:18:22 +0100

    Revert "fix: [security] upgraded jquery-ui used by gridstack, fixes #10531"
    This reverts commit c5cd3657e4d74bce34f25d1c74d86f58dfb1cb90.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -34,7 +34,7 @@ class AppController extends Controller
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
     private $__queryVersion = '177';
-    public $pyMispVersion = '2.5.17.1';
+    public $pyMispVersion = '2.5.10';
     public $phpmin = '7.2';
     public $phprec = '7.4';
     public $phptoonew = '8.0';
