7ebfce1ddcee6ddd62b19f2600e960f15cab0ec6
Xavier Claude <xavier.claude@circl.lu>
2026-03-19 18:17:14 +0100

    Allow to rerun the database initialization
    Don't recreate the user or the db and only import the schema if the
    database is empty

--- INSTALL/INSTALL.ubuntu2404.sh
+++ INSTALL/INSTALL.ubuntu2404.sh
@@ -365,13 +365,16 @@ fi
 DBCONN_ADMIN_STRING="${DBPORT_STRING} ${DBHOST_STRING} ${DBUSER_ADMIN_STRING} ${DBPASSWORD_ADMIN_STRING}"
 DBCONN_MISP_STRING="${DBPORT_STRING} ${DBHOST_STRING} ${DBUSER_MISP_STRING} ${DBPASSWORD_MISP_STRING}"
 
-sudo mysql $DBCONN_ADMIN_STRING -e "CREATE DATABASE ${DBNAME};"  &>> $logfile
-sudo mysql $DBCONN_ADMIN_STRING -e "CREATE USER '${DBUSER_MISP}'@'localhost' IDENTIFIED BY '${DBPASSWORD_MISP}';"  &>> $logfile
+sudo mysql $DBCONN_ADMIN_STRING -e "CREATE DATABASE IF NOT EXISTS ${DBNAME};" &>>$logfile
+sudo mysql $DBCONN_ADMIN_STRING -e "CREATE USER IF NOT EXISTS '${DBUSER_MISP}'@'localhost' IDENTIFIED BY '${DBPASSWORD_MISP}';" &>>$logfile
 sudo mysql $DBCONN_ADMIN_STRING -e "GRANT USAGE ON *.* to '${DBUSER_MISP}'@'localhost';"  &>> $logfile
 sudo mysql $DBCONN_ADMIN_STRING -e "GRANT ALL PRIVILEGES on ${DBNAME}.* to '${DBUSER_MISP}'@'localhost';"  &>> $logfile
 sudo mysql $DBCONN_ADMIN_STRING -e "FLUSH PRIVILEGES;"  &>> $logfile
-mysql $DBCONN_MISP_STRING $DBNAME < "${MISP_PATH}/INSTALL/MYSQL.sql"  &>> $logfile
-error_check "MISP database schema import"
+
+if [ "$(mysql -Nse "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$DBNAME';")" -eq 0 ]; then
+    mysql $DBCONN_MISP_STRING $DBNAME <"${MISP_PATH}/INSTALL/MYSQL.sql" &>>$logfile
+    error_check "MISP database schema import"
+fi
 
 print_status "Moving and configuring MISP php config files.."
 
