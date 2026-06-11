1ab44b01e5f616eafb1d7b26986d79e0761db8c2
iglocska <andras.iklody@gmail.com>
2026-06-11 16:01:02 +0200

    fix: [security] sharing groups mass assignment fix in add
    - as reported by Jeroen Pinoy
    
    (cherry picked from commit 687e7cb530ae0e2faaadf5e3e44712258fb3ef1b)

--- app/Controller/SharingGroupsController.php
+++ app/Controller/SharingGroupsController.php
@@ -78,6 +78,10 @@ class SharingGroupsController extends AppController
                 }
             }
             $this->SharingGroup->create();
+            // Never allow an id to be supplied on add: a primary key in the save data turns
+            // create() + save() into an update of an arbitrary existing sharing group, bypassing
+            // the edit ACL and letting a user take over a SG they have no access to.
+            unset($sg['id']);
             if (!$canModifyUuid) {
                 unset($sg['uuid']);
             }
