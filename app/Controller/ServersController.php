5902b249d5be5355dca7ee0f5c4fe26e4ad85221
Jeroen Pinoy <pinoy.jeroen@gmail.com>
2026-07-08 11:30:09 +0200

    fix: [ServersController:updateJSON] correctly return update results when not using async update
    Fixes a typo that caused "results" to be null for all updates

--- app/Controller/ServersController.php
+++ app/Controller/ServersController.php
@@ -2213,7 +2213,7 @@ public function updateJSON()
             $results['message'] = __('Server updateJSON job queued. Job ID: %s', $jobId);
         } else {
             foreach ($this->Server->updateJSON() as $type => $result) {
-                $results[$type] = $results['success'];
+                $results[$type] = $result['success'];
             }
         }
 
