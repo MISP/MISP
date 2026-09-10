0b0571a877aa4a2fa69a4cdcdbafd12ee553bbc9
ThomasLcr <thomas.lacroix@circl.lu>
2026-09-10 11:33:12 +0200

    new: [Overmind] Defend MISP from invaders

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -40,7 +40,7 @@ class AppController extends Controller
      */
     const PRE_AUTH_FLOOD_WINDOW = 900;
 
-    private $__queryVersion = '218';
+    private $__queryVersion = '219';
     public $pyMispVersion = '2.5.34.2';
     public $phpmin = '8.1';
     public $phprec = '8.2';
