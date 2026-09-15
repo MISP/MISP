9102131787ba743e3594d7aca114839e2d02025f
iglocska <andras.iklody@gmail.com>
2026-09-15 15:52:21 +0200

    fix: [security] Harden misp modules module choice requirements
    - don't allow querying legacy modules
    - as reported by Jeroen Pinoy
    
    (cherry picked from commit 7dfcc4d32970d553e8c39827dac04889e44f94e0)

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -5484,8 +5484,10 @@ class EventsController extends AppController
         } else {
             $options = [];
             $format = 'simplified';
+            $moduleFound = false;
             foreach ($enabledModules['modules'] as $temp) {
                 if ($temp['name'] == $module) {
+                    $moduleFound = true;
                     $format = !empty($temp['mispattributes']['format']) ? $temp['mispattributes']['format'] : 'simplified';
                     if (isset($temp['meta']['config'])) {
                         foreach ($temp['meta']['config'] as $conf) {
@@ -5494,6 +5496,9 @@ class EventsController extends AppController
                     }
                     break;
                 }
+            }
+            if (!$moduleFound) {
+                throw new MethodNotAllowedException(__('Module not found or not available.'));
             }
             $distributions = $this->Event->Attribute->distributionLevels;
             $sgs = $this->Event->SharingGroup->fetchAllAuthorised($this->Auth->user(), 'name', 1);
