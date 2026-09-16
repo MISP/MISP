407ce8d2266bafcac6df3b48066b760a50ba16b1
ThomasLcr <thomas.lacroix@circl.lu>
2026-09-16 15:08:12 +0200

    new: [Overmind] Added new endpoint for Collection modal

--- app/Lib/Tools/OvermindPages.php
+++ app/Lib/Tools/OvermindPages.php
@@ -25,7 +25,7 @@ class OvermindPages
         'event_reports' => array('index', 'view', 'add', 'edit'),
         'EventReportTemplateVariables' => array('index', 'add', 'edit'),
         'collections' => array('index', 'view', 'add', 'edit'),
-        'CollectionElements' => array('add', 'index'),
+        'CollectionElements' => array('add', 'index', 'addElementToCollection'),
         'analystData' => array(
             'index', 'add', 'edit', 'view', 'delete', 'deleteSelection'
         ),
